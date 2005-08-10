c1361a4abf905b652a8447d07f78b3f2cef0573d
larsjo <larsjo@sesam.no>
2005-08-10 09:27:30 +0000

    added template for webcrawl
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@21 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/java/no/schibstedsok/front/searchportal/util/VelocityTemplates.java
+++ src/java/no/schibstedsok/front/searchportal/util/VelocityTemplates.java
@@ -17,6 +17,7 @@ public final class VelocityTemplates {
 	public static final String ALL_COLLECTIONS_SEARCH = "fast-search-result.vm";			
 	public static final String WIKI_COLLECTION_SEARCH = "fast-wiki-search-result.vm";			
 	public static final String MEDIA_COLLECTION_SEARCH = "fast-retriever-search-result.vm";	
+	public static final String WEBCRAWL_COLLECTION_SEARCH = "fast-search-result.vm";	//TODO: refactor into new template after design finished.
 	public static final String GENERIC_SEARCH = "search-results.vm";			
 	public static final String GLOBAL_SEARCH = "sensis-search-result.vm";
 	public static final String GLOBAL_COUNT = "global-counter.vm";
@@ -24,4 +25,5 @@ public final class VelocityTemplates {
 	public static final String TV_SEARCH = "tv-enrich.vm";
 
     public static final String COMPANIES_COLLECTION_SEARCH = "fast-companies-search-result.vm";
+	
 }
