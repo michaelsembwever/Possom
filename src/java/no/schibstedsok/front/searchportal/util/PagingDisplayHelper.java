37301c3f14b2fab7c2ce05c0a9bc5b8603122c2b
magnuse <magnuse@sesam.no>
2005-08-12 07:42:14 +0000

    Fixed bug that caused the current page to be "one off".
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@34 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/java/no/schibstedsok/front/searchportal/util/PagingDisplayHelper.java
+++ src/java/no/schibstedsok/front/searchportal/util/PagingDisplayHelper.java
@@ -12,7 +12,6 @@ public class PagingDisplayHelper {
 
     private int pageSize = 10;
     private int maxPages = 10;
-    private int windowSize = 3;
 
     private int numberOfResults;
     private int currentPage;
@@ -46,10 +45,6 @@ public class PagingDisplayHelper {
         return numberOfResults / pageSize + 1;
     }
 
-    public int getNumberOfPagesLimited() {
-        return getNumberOfPages() > maxPages ? maxPages : getNumberOfPages();
-    }
-
     public boolean isFirstPage() {
         return currentPage == 1;
     }
@@ -59,42 +54,29 @@ public class PagingDisplayHelper {
     }
 
     public void setCurrentOffset(int offset) {
-        currentPage = offset / pageSize + 1;
+        currentPage = offset / (pageSize + 1) + 1;
     }
 
     public int getOffsetOfNextPage() {
-        return currentPage * pageSize + 1;
+        return currentPage * (pageSize + 1);
     }
 
     public int getOffsetOfPreviousPage() {
         return (currentPage - 2) * pageSize;
     }
 
-
     public int getOffsetOfPage(int page) {
-        return (page - 1) * pageSize;
+        return (page - 1) * (pageSize + 1);
     }
 
     public int getFirstVisiblePage() {
-        int first = (getCurrentPage() - 1) / pageSize * maxPages + 1;
-
-        if (first != 1) {
-            first -= windowSize;
-        }
-
-        return first;
+        return (getCurrentPage() - 1) / pageSize * maxPages + 1;
     }
 
     public int getLastVisiblePage() {
-        int pageSet = getFirstVisiblePage() + maxPages;
+        int pageSet = getFirstVisiblePage() + maxPages - 1;
 
-        int first = getFirstVisiblePage();
-
-        if (first != 1) {
-            pageSet += windowSize;
-        }
-
-        return getNumberOfPages()  < pageSet ? getNumberOfPages() : pageSet;
+        return getNumberOfPages() < pageSet ? getNumberOfPages() : pageSet;
     }
 
     public int getMaxPages() {
