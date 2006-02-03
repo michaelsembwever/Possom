bce5576285b7d74cbbb334bfb8d8b5b5adcd7032
michaelsembwever <mick@semb.wever.org>
2006-02-03 12:48:30 +0000

    forgot to add. ooops. not my day :$
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@2177 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ src/java/no/schibstedsok/front/searchportal/configuration/loader/PropertiesContext.java
@@ -0,0 +1,24 @@
+/* Copyright (2005-2006) Schibsted Søk AS
+ *
+ * Created on 23 January 2006, 13:54
+ */
+
+package no.schibstedsok.front.searchportal.configuration.loader;
+
+import com.thoughtworks.xstream.XStream;
+import java.util.Properties;
+import javax.xml.parsers.DocumentBuilder;
+
+/** Defines the context for consumers of PropertiesLoaders.
+ *
+ * @version $Id: ResourceContext.java 2045 2006-01-25 12:10:01Z mickw $
+ * @author <a href="mailto:mick@wever.org">Michael Semb Wever</a>
+ */
+public interface PropertiesContext {
+    /** Create a new PropertiesLoader for the given resource name/path and load it into the given properties.
+     * @param resource the resource name/path.
+     * @param properties the properties to hold the individual properties loaded.
+     * @return the new PropertiesLoader to use.
+     **/
+    PropertiesLoader newPropertiesLoader(String resource, Properties properties);
+}
---
+++ src/java/no/schibstedsok/front/searchportal/configuration/loader/XStreamContext.java
@@ -0,0 +1,26 @@
+/* Copyright (2005-2006) Schibsted Søk AS
+ *
+ * Created on 23 January 2006, 13:54
+ */
+
+package no.schibstedsok.front.searchportal.configuration.loader;
+
+import com.thoughtworks.xstream.XStream;
+import java.util.Properties;
+import javax.xml.parsers.DocumentBuilder;
+
+/** Defines the context for consumers of XStreamLoaders.
+ *
+ * @version $Id: ResourceContext.java 2045 2006-01-25 12:10:01Z mickw $
+ * @author <a href="mailto:mick@wever.org">Michael Semb Wever</a>
+ */
+public interface XStreamContext {
+
+    /** Create a new XStreamLoader for the given resource name/path and load it with the given XStream.
+     * @param resource the resource name/path.
+     * @param xstream the xstream to deserialise the resource with.
+     * @return the new PropertiesLoader to use.
+     **/
+    XStreamLoader newXStreamLoader(String resource, XStream xstream);
+
+}
