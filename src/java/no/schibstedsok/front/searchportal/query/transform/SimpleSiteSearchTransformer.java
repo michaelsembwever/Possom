0d7b496b4c203e368959f29bd45f2ec6503f28ec
michaelsembwever <mick@semb.wever.org>
2006-03-29 21:20:34 +0000

    - default list in SimpleSiteSearchTransformer. - move dtds to search-front-html where they can be published from.
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@2659 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/java/no/schibstedsok/front/searchportal/query/transform/SimpleSiteSearchTransformer.java
+++ src/java/no/schibstedsok/front/searchportal/query/transform/SimpleSiteSearchTransformer.java
@@ -8,6 +8,9 @@
 
 package no.schibstedsok.front.searchportal.query.transform;
 
+
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -22,21 +25,47 @@ import java.util.Map;
  * <code>parameterName</code>.
  *
  * @author <a href="mailto:magnus.eklund@sesam.no">Magnus Eklund</a>
- * @version <tt>$Revision$</tt>
+ * @version <tt>$Id$</tt>
  */
 public final class SimpleSiteSearchTransformer extends AbstractQueryTransformer implements QueryTransformer {
+
+    private static final Map<String,String> DEFAULT_SITES;
+
+    static{
+        final Map<String,String> defaultSites = new HashMap<String,String>();
+        defaultSites.put("ds", "dinside.no");
+        defaultSites.put("it", "itavisen.no");
+        defaultSites.put("di", "digi.no");
+        defaultSites.put("pr", "propaganda-as.no");
+        defaultSites.put("im", "imarket.no");
+        defaultSites.put("nrk", "nrk.no");
+        defaultSites.put("af", "aftenposten.no");
+        defaultSites.put("fv", "fedrelandsvennen.no");
+        defaultSites.put("aa", "adressa.no");
+        defaultSites.put("bt", "bt.no");
+
+        DEFAULT_SITES = Collections.unmodifiableMap(defaultSites);
+    }
 
     private Map<String,String> sites;
     private String parameterName;
     private String filterName;
 
     public String getFilter(final Map parameters) {
-      
+
         final String[] paramValue = (String[]) parameters.get(parameterName);
 
         if (paramValue != null && paramValue.length > 0) {
             if (!(paramValue[0].equals("") || paramValue[0].equals("d"))) {
-                if (sites.containsKey(paramValue[0])) {
+
+                final Map<String,String> s = sites != null && sites.size()>0
+                        ? new HashMap(DEFAULT_SITES)
+                        : DEFAULT_SITES;
+                if( s != DEFAULT_SITES ){
+                    s.putAll(sites);
+                }
+
+                if (s.containsKey(paramValue[0])) {
                     final StringBuffer filter = new StringBuffer("+");
                     filter.append(filterName);
                     filter.append(':');
@@ -47,7 +76,7 @@ public final class SimpleSiteSearchTransformer extends AbstractQueryTransformer 
         }
         return null;
     }
-    
+
     public Object clone() throws CloneNotSupportedException {
         final SimpleSiteSearchTransformer retValue = (SimpleSiteSearchTransformer)super.clone();
         retValue.parameterName = parameterName;
