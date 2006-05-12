8b0510117de68b717ecda5084acfbcbbf5313d64
michaelsembwever <mick@semb.wever.org>
2006-05-12 10:41:38 +0000

    should have been added
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@2897 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ src/java/no/schibstedsok/front/searchportal/view/velocity/ChopStringDirective.java
@@ -0,0 +1,93 @@
+package no.schibstedsok.front.searchportal.view.velocity;
+
+import java.io.IOException;
+
+import java.io.Writer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.context.InternalContextAdapter;
+import org.apache.velocity.exception.MethodInvocationException;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.apache.velocity.runtime.directive.Directive;
+import org.apache.velocity.runtime.parser.Token;
+import org.apache.velocity.runtime.parser.node.Node;
+
+/**
+ *
+ * A velocity directive to chop a string
+ *
+ * <code>
+ * #chopString('this string is being chopped!' 20)
+ * returns the string: "this string is.."
+ * </code>
+ *
+ *
+ * @author thomas
+ */
+public final class ChopStringDirective extends Directive {
+
+    private static transient Log log = LogFactory.getLog(ChopStringDirective.class);
+
+    private static final String NAME = "chopString";
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getName() {
+        return NAME;
+    }
+
+   /**
+     * {@inheritDoc}
+     */
+    public int getType() {
+        return LINE;
+    }
+
+   /**
+     * {@inheritDoc}
+     */
+    public boolean render(final InternalContextAdapter context, final Writer writer, final Node node) throws IOException, ResourceNotFoundException, ParseErrorException, MethodInvocationException {
+        if (node.jjtGetNumChildren() != 2) {
+            rsvc.error("#" + getName() + " - wrong number of arguments");
+            return false;
+        }
+
+        final String s = node.jjtGetChild(0).value(context).toString();
+        final int length = Integer.parseInt(node.jjtGetChild(1).value(context).toString());
+
+        String choppedString = "";
+        if (s.length() <= length)
+            choppedString = s;
+        else {
+            final String sub = s.substring(0, length);
+            final String lastChar = Character.toString(sub.charAt(sub.length() - 1));
+            if (lastChar.equals("."))
+                choppedString = sub.substring(0, length) + "..";
+            else if (lastChar.equals(" "))
+                choppedString = sub.substring(0, length) + " ...";
+            else {
+		final int lastSpace = sub.lastIndexOf(" ");
+
+		if (lastSpace >= 0) {
+		    choppedString = sub.substring(0, sub.lastIndexOf(" ")) + " ...";
+		} else {
+		    choppedString = sub.substring(0, length) + "...";
+		}
+	    }
+        }
+
+        writer.write(choppedString);
+
+        final Token lastToken = node.getLastToken();
+
+        if (lastToken.image.endsWith("\n")) {
+            writer.write("\n");
+        }
+
+        return true;
+
+    }
+}
