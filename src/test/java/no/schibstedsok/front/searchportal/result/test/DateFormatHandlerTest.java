bd29954feb97c0c61ca38938bb8a1b7810adcaf8
ajamtli <ajamtli@sesam.no>
2006-05-19 21:47:33 +0000

    DateFormatHandler test
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@2949 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ src/test/java/no/schibstedsok/front/searchportal/result/test/DateFormatHandlerTest.java
@@ -0,0 +1,96 @@
+// Copyright (2006) Schibsted Søk AS
+package no.schibstedsok.front.searchportal.result.test;
+
+import junit.framework.TestCase;
+import no.schibstedsok.common.ioc.BaseContext;
+import no.schibstedsok.common.ioc.ContextWrapper;
+import no.schibstedsok.front.searchportal.query.Query;
+import no.schibstedsok.front.searchportal.result.BasicSearchResult;
+import no.schibstedsok.front.searchportal.result.BasicSearchResultItem;
+import no.schibstedsok.front.searchportal.result.SearchResult;
+import no.schibstedsok.front.searchportal.result.handler.DateFormatHandler;
+import no.schibstedsok.front.searchportal.result.handler.ResultHandler;
+import no.schibstedsok.front.searchportal.view.config.SearchTab;
+import org.apache.log4j.Logger;
+
+import static no.schibstedsok.front.searchportal.result.handler.DateFormatHandler.Fields;
+
+/** Fast navigation tests.
+ *
+ * @author <a href="mailto:magnus.eklund@schibsted.no">Magnus Eklund</a>
+ * @version <tt>$Revision: 2859 $</tt>
+ */
+public final class DateFormatHandlerTest extends TestCase {
+
+    private static final Logger LOG = Logger.getLogger(DateFormatHandlerTest.class);
+
+    private static final String SOURCE_FIELD = "source_field";
+    private static final String FIELD_PREFIX = "prefix";
+            
+    final MockupSearchCommand command = new MockupSearchCommand();
+    final BasicSearchResult bsr = new BasicSearchResult(command);
+    final ResultHandler.Context resultHandlerContext = ContextWrapper.wrap(
+            ResultHandler.Context.class,
+            new BaseContext(){
+                public SearchResult getSearchResult() {
+                    return bsr;
+                }
+                
+                public SearchTab getSearchTab() {
+                    return null;
+                }
+                
+                public String getQueryString() {
+                    return null;
+                }
+                
+                public Query getQuery() {
+                    return null;
+                }
+            }
+    );
+    
+    DateFormatHandler rh = null;
+    
+    protected void setUp() throws Exception {
+        rh = new DateFormatHandler();
+        rh.setSourceField(SOURCE_FIELD);
+        
+        resultHandlerContext.getSearchResult().addResult(createItem("2006-04-27T10:11:12Z"));
+    }
+
+    private BasicSearchResultItem createItem(String time) {
+        BasicSearchResultItem bsri = new BasicSearchResultItem();
+        bsri.addField(SOURCE_FIELD, time);
+        return bsri;
+    }
+    
+    public void testOneWithoutPrefix() {
+        
+        rh.handleResult(resultHandlerContext, null);
+        
+        assertEquals(1, resultHandlerContext.getSearchResult().getResults().size());
+        BasicSearchResultItem bsri = (BasicSearchResultItem) resultHandlerContext.getSearchResult().getResults().get(0);
+        
+        assertEquals("2006", bsri.getField(Fields.YEAR.name()));
+        assertEquals("04", bsri.getField(Fields.MONTH.name()));
+        assertEquals("27", bsri.getField(Fields.DAY.name()));
+        assertEquals("10", bsri.getField(Fields.HOUR.name()));
+        assertEquals("11", bsri.getField(Fields.MINUTE.name()));
+        assertEquals("12", bsri.getField(Fields.SECOND.name()));
+    }
+
+    public void testOneWithPrefix() {
+        rh.setFieldPrefix(FIELD_PREFIX);
+        rh.handleResult(resultHandlerContext, null);
+        
+        assertEquals(1, resultHandlerContext.getSearchResult().getResults().size());
+        BasicSearchResultItem bsri = (BasicSearchResultItem) resultHandlerContext.getSearchResult().getResults().get(0);
+        assertEquals("2006", bsri.getField(FIELD_PREFIX + Fields.YEAR.name()));
+        assertEquals("04", bsri.getField(FIELD_PREFIX + Fields.MONTH.name()));
+        assertEquals("27", bsri.getField(FIELD_PREFIX + Fields.DAY.name()));
+        assertEquals("10", bsri.getField(FIELD_PREFIX + Fields.HOUR.name()));
+        assertEquals("11", bsri.getField(FIELD_PREFIX + Fields.MINUTE.name()));
+        assertEquals("12", bsri.getField(FIELD_PREFIX + Fields.SECOND.name()));
+    }
+}
