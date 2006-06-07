bb9620bd1838e5a43e32d38d35be2fc155620f52
larsjo <larsjo@sesam.no>
2006-06-07 11:14:38 +0000

    new files for weather search
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3070 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ src/java/no/schibstedsok/front/searchportal/configuration/StormWeatherSearchConfiguration.java
@@ -0,0 +1,40 @@
+// Copyright (2006) Schibsted Søk AS
+/*
+ * NewsSearchConfiguration.java
+ *
+ * Created on March 7, 2006, 5:42 PM
+ *
+ */
+
+package no.schibstedsok.front.searchportal.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author larsj
+ */
+public class StormWeatherSearchConfiguration extends FastConfiguration {
+
+	private List<String> elementValues = new ArrayList<String>();
+	
+	public StormWeatherSearchConfiguration(){
+        super(null);
+    }
+
+    public StormWeatherSearchConfiguration(final SearchConfiguration asc){
+        super(asc);
+    }
+
+	public List<String> getElementValues() {
+		return elementValues;
+	}
+
+	public void addElementValue(String string) {
+		this.elementValues.add(string);
+	}
+	
+    
+}
