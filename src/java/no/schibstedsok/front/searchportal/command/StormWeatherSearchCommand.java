bbe35804ebab48b685766895571586ffec90a859
larsjo <larsjo@sesam.no>
2006-06-13 15:08:55 +0000

    added default / no query handling
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3121 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/java/no/schibstedsok/front/searchportal/command/StormWeatherSearchCommand.java
+++ src/java/no/schibstedsok/front/searchportal/command/StormWeatherSearchCommand.java
@@ -2,11 +2,13 @@
 package no.schibstedsok.front.searchportal.command;
 
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.Map;
+
 import no.schibstedsok.front.searchportal.InfrastructureException;
-import no.schibstedsok.front.searchportal.configuration.FastNavigator;
 import no.schibstedsok.front.searchportal.configuration.StormWeatherSearchConfiguration;
-import no.schibstedsok.front.searchportal.configuration.loader.FileResourceLoader;
-import no.schibstedsok.front.searchportal.configuration.loader.PropertiesLoader;
 import no.schibstedsok.front.searchportal.http.HTTPClient;
 import no.schibstedsok.front.searchportal.result.BasicSearchResult;
 import no.schibstedsok.front.searchportal.result.BasicSearchResultItem;
@@ -14,28 +16,15 @@ import no.schibstedsok.front.searchportal.result.FastSearchResult;
 import no.schibstedsok.front.searchportal.result.SearchResult;
 import no.schibstedsok.front.searchportal.result.SearchResultItem;
 import no.schibstedsok.front.searchportal.util.SearchConstants;
-import org.apache.commons.httpclient.HttpException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
-import org.xml.sax.SAXException;
-import com.opensymphony.oscache.general.GeneralCacheAdministrator;
+
 import com.opensymphony.oscache.base.NeedsRefreshException;
-
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.net.URLEncoder;
+import com.opensymphony.oscache.general.GeneralCacheAdministrator;
 
 /**
  * @author <a href="mailto:lars.johansson@conduct.no">Lars Johansson</a>
@@ -58,7 +47,9 @@ public class StormWeatherSearchCommand extends FastSearchCommand {
      */
     public StormWeatherSearchCommand(final SearchCommand.Context cxt, final Map parameters) {
         super(cxt, parameters);
-    }
+        log.debug("Creating WeatherSearchCommand");
+
+    } 
     
     protected FastSearchResult executeFastCommand(){
     	return (FastSearchResult) super.execute();
@@ -68,11 +59,25 @@ public class StormWeatherSearchCommand extends FastSearchCommand {
      * First do a Fast search to get the lat/long of the place in question,
      * then use fast navigator values to query the Storm weather service. 
      * 
+     * 
+     * The model is that a location may carry one or more forecasts.
+     * Each resultItem carries forecasts in a nested searchResult named
+     * with the key "forecasts".
+     * 
+     * Location is in FAST index, forecasts is fetched from Storm.
+     * 
+     * 
      */
     public SearchResult execute() {
 
     	//Fast search
         FastSearchResult fastResult = executeFastCommand();
+        
+        //on empty queries return only the navigators
+        if(getRunningQuery().getQuery().isBlank()){
+        	fastResult.setHitCount(0);
+        	return fastResult;
+        }
         
     	// "enrich" the Fast result with Storm weather forecasts based on lat/long.
         if(fastResult.getResults().size() > 0){
@@ -89,7 +94,8 @@ public class StormWeatherSearchCommand extends FastSearchCommand {
         		String lat = result.getField("lat");
         		String lon = result.getField("long");
         		
-        			if(getParameter("igeneric1") != null &! "".equals(getParameter("igeneric1"))){
+        			//infopage or resultlisting?
+        		   if(getParameter("igeneric1") != null &! "".equals(getParameter("igeneric1"))){
 
         				getForecasts(forecasts, lat, lon);
         		        
@@ -104,10 +110,8 @@ public class StormWeatherSearchCommand extends FastSearchCommand {
         		result.addNestedSearchResult("forecasts", forecasts);
         	}
         	
-        } else {
-        	log.debug("No navigators found");
         }
-
+        
         return fastResult;
         
     }
@@ -130,7 +134,7 @@ public class StormWeatherSearchCommand extends FastSearchCommand {
     	
     	} catch (NeedsRefreshException nre) {
     		
-    		log.info("Refreshing cache for " + cacheKey);
+    		log.debug("Refreshing cache for " + cacheKey);
     		
     		try {
 
@@ -158,12 +162,12 @@ public class StormWeatherSearchCommand extends FastSearchCommand {
 	    			e = (BasicSearchResultItem) nre.getCacheContent();
 	    			log.error("Cache update exception, forecasts may become stale! " + ex.getMessage());
 	    		} finally{
-    			if (!updated) {
-    				// It is essential that cancelUpdate is called if the
-    				// cached content could not be rebuilt
-    				admin.cancelUpdate(cacheKey);
-    			}
-    		}
+	    			if (!updated) {
+	    				// It is essential that cancelUpdate is called if the
+	    				// cached content could not be rebuilt
+	    				admin.cancelUpdate(cacheKey);
+	    			}
+	    		}
     	}
     	
         return e;
@@ -247,11 +251,15 @@ public class StormWeatherSearchCommand extends FastSearchCommand {
     private BasicSearchResultItem getItem(Element element) {
 
 		BasicSearchResultItem e = new BasicSearchResultItem();
-		for (Iterator iter = ((StormWeatherSearchConfiguration)context.getSearchConfiguration()).getElementValues().iterator(); iter.hasNext();) {
+		for (Iterator iter = getConfiguration().getElementValues().iterator(); iter.hasNext();) {
 			String field = (String) iter.next();
 			e.addField(field, getTextValue(element, field));
 		}
 		return e;
+	}
+
+	private StormWeatherSearchConfiguration getConfiguration() {
+		return (StormWeatherSearchConfiguration)context.getSearchConfiguration();
 	}
 
 
@@ -266,16 +274,47 @@ public class StormWeatherSearchCommand extends FastSearchCommand {
 	}
 
 	private Document doSearch(final String url) {
-        try {
-            return client.getXmlDocument("weather", url);
-        } catch (HttpException e) {
-            log.error("Unable to connect to " + url, e);
-        } catch (IOException e) {
-            log.error("Problems with connection to " + url, e);
-        } catch (SAXException e) {
-            log.error("XML Parse error " + url , e);
-        }
-        return null;
+        
+		Document doc = null;
+		String cacheKey = url;
+		
+    	
+		boolean updated = false; //cache flag used for eviction/update deadlock.
+    
+    	try {
+        	
+    		// Get from the cache
+    		doc = (Document) admin.getFromCache(cacheKey, EVICTIONPERIOD_WEATHER_CACHE);
+    	
+    	} catch (NeedsRefreshException nre) {
+    		
+    		try {
+
+    			// Get from Storm service  
+            	doc = client.getXmlDocument("weather", url);
+
+    			// Store in the cache
+    			admin.putInCache(cacheKey, doc);
+
+    			updated = true;
+
+	    		} catch (Exception ex) {
+	
+	    			// We have the outdated content for fail-over. May become stale!
+	    			doc = (Document) nre.getCacheContent();
+	    			log.error("Cache update exception, document may become stale! " + ex.getMessage());
+
+	    		} finally{
+	    			if (!updated) {
+	    				// It is essential that cancelUpdate is called if the
+	    				// cached content could not be rebuilt
+	    				admin.cancelUpdate(cacheKey);
+	    			}
+	    		}
+    		
+    	}
+        	
+        return doc;
     }
 	
 }
