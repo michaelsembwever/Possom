e29948aac9cdc95fb5fc8d2232d1e5007e4b7829
michaelsembwever <mick@semb.wever.org>
2006-06-23 11:57:17 +0000

    SEARCH-705
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3200 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/java/no/schibstedsok/front/searchportal/query/parser/PhoneNumberClauseImpl.java
+++ src/java/no/schibstedsok/front/searchportal/query/parser/PhoneNumberClauseImpl.java
@@ -64,7 +64,8 @@ public final class PhoneNumberClauseImpl extends AbstractLeafClause implements P
         final TokenEvaluatorFactory predicate2evaluatorFactory) {
 
         // remove all embedded whitespace
-        final String t = term.replaceAll(" ","").replaceFirst("^(\\+|00)?47","");
+        String t = term.replaceAll(" ","");
+        t = t.length() > 8 ? t.replaceFirst("^(\\+|00)?47","") : t;
         // update the factory with what the current term is
         predicate2evaluatorFactory.setCurrentTerm(term);
         // the weakCache to use.
