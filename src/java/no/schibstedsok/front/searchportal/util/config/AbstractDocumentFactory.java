7b23c1368aebf6be253a6e5ceba0507fa8218d9a
michaelsembwever <mick@semb.wever.org>
2006-07-01 20:45:33 +0000

    utility method to use when unpacking matching xml attributes into bean properties. not yet put into use.
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3267 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/java/no/schibstedsok/front/searchportal/util/config/AbstractDocumentFactory.java
+++ src/java/no/schibstedsok/front/searchportal/util/config/AbstractDocumentFactory.java
@@ -7,7 +7,12 @@
 
 package no.schibstedsok.front.searchportal.util.config;
 
+import java.lang.reflect.InvocationTargetException;
 import no.schibstedsok.front.searchportal.configuration.loader.PropertiesContext;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+
 
 /**
  *
@@ -18,6 +23,8 @@ public abstract class AbstractDocumentFactory {
     
     
     // Constants -----------------------------------------------------
+    private static final Logger LOG = Logger.getLogger(AbstractDocumentFactory.class);
+    private static final String ERR_FILL_PROPERTY = "Could not set javabean property";
     
     // Attributes ----------------------------------------------------
     
@@ -41,6 +48,48 @@ public abstract class AbstractDocumentFactory {
     
     // Private -------------------------------------------------------
     
+    enum ParseType{
+        Boolean, Int, Float, String, Property
+    };
+    
+    protected static final void fillBeanProperty(
+            final Object beanObj, 
+            final Object beanParent, 
+            final String property,
+            final ParseType type,
+            final Element element,
+            final String def){
+        
+      try {
+            
+            final StringBuilder attributeName = new StringBuilder(property);
+            for(int i = 0; i < attributeName.length(); ++i){
+                final char c = attributeName.charAt(i);
+                if(Character.isUpperCase(c)){
+                    attributeName.replace(i, i+1, "-" + Character.toLowerCase(c));
+                    ++i;
+                }
+            }
+            
+            final String attributeValue = element.getAttribute(attributeName.toString());
+            final String inheritedValue = BeanUtils.getProperty(beanParent, property);
+            
+            if( attributeValue != null && attributeValue.length()>0 ){
+                BeanUtils.setProperty(beanObj, property, attributeValue);
+            }else if( inheritedValue != null && inheritedValue.length()>0 ){
+                BeanUtils.setProperty(beanObj, property, inheritedValue);
+            }else{
+                BeanUtils.setProperty(beanObj, property, def);
+            }
+            
+        } catch (IllegalAccessException ex) {
+            LOG.error(ERR_FILL_PROPERTY, ex);
+        } catch (NoSuchMethodException ex) {
+            LOG.error(ERR_FILL_PROPERTY, ex);
+        } catch (InvocationTargetException ex) {
+            LOG.error(ERR_FILL_PROPERTY, ex);
+        }        
+    }
     
     protected static final boolean parseBoolean(final String s, final boolean def){
         return s.trim().length() == 0 ? def : Boolean.parseBoolean(s);
