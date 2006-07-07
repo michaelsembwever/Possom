e6aea126ee14e3ae6b2dc4ee0423ac32401364b3
ajamtli <ajamtli@sesam.no>
2006-07-07 12:31:12 +0000

    quickfix for sorting on day... using one search for each day
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3288 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/java/no/schibstedsok/front/searchportal/command/TvSearchCommand.java
+++ src/java/no/schibstedsok/front/searchportal/command/TvSearchCommand.java
@@ -12,6 +12,7 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import no.schibstedsok.front.searchportal.configuration.TvSearchConfiguration;
+import no.schibstedsok.front.searchportal.result.BasicSearchResult;
 import no.schibstedsok.front.searchportal.result.SearchResult;
 import no.schibstedsok.front.searchportal.result.SearchResultItem;
 import org.apache.log4j.Logger;
@@ -25,7 +26,8 @@ public class TvSearchCommand extends AbstractSimpleFastSearchCommand {
     private static final Logger LOG = Logger.getLogger(TvSearchCommand.class);
 
     protected StringBuilder defaultChannelFilter = new StringBuilder();
-
+    private String additionalFilter;
+    
     /** Creates a new instance of TvSearchCommand
      *
      * @param cxt Search command context.
@@ -43,10 +45,15 @@ public class TvSearchCommand extends AbstractSimpleFastSearchCommand {
                 defaultChannelFilter.append(channel);
             }
             defaultChannelFilter.append(")");
+            additionalFilter = "";
         }
     }
 
     protected int getResultsToReturn() {
+        final String sortByString = this.getParameters().get("userSortBy") != null ? (String) this.getParameters().get("userSortBy") : "channel";
+        if ("day".equals(sortByString)) {
+            return 15;
+        }
         if (getRunningQuery().getQuery().isBlank()) {
             return getTvSearchConfiguration().getResultsToFetch();
         } else {
@@ -55,7 +62,22 @@ public class TvSearchCommand extends AbstractSimpleFastSearchCommand {
     }
     
     public SearchResult execute() {
-        SearchResult sr = super.execute();
+        final String sortByString = this.getParameters().get("userSortBy") != null ? (String) this.getParameters().get("userSortBy") : "channel";
+        SearchResult sr = null;
+        
+        if ("day".equals(sortByString)) {
+            sr = new BasicSearchResult(this);
+            int totalHits = 0;
+            for (int i = 0; i < 7; i++) {
+                setAdditionalFilter(" +sgeneric7nav:" + i);
+                SearchResult result = super.execute();
+                sr.getResults().addAll(result.getResults());
+                totalHits += result.getHitCount();
+            }
+            sr.setHitCount(totalHits);
+        } else {
+            sr = super.execute();
+        }
 
         return sr;
     }
@@ -67,13 +89,17 @@ public class TvSearchCommand extends AbstractSimpleFastSearchCommand {
     protected String getAdditionalFilter() {
         /* Only fetch default channels on blank query */
         if (getRunningQuery().getQuery().isBlank() && !getParameters().containsKey("nav_channels") && !getParameters().containsKey("nav_categories")) {
-            return defaultChannelFilter.toString();
+            return additionalFilter + " " + defaultChannelFilter.toString();
         }
         
         if (getRunningQuery().getQuery().isBlank() && getParameters().containsKey("output") && getParameters().get("output").equals("rss")) {
-            return defaultChannelFilter.toString();
+            return additionalFilter + " " + defaultChannelFilter.toString();
         }
         
-        return "";
+        return additionalFilter;
+    }
+    
+    protected void setAdditionalFilter(String additionalFilter) {
+        this.additionalFilter = additionalFilter;
     }
 }
