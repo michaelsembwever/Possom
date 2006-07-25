0e1b2f9c0cb274ee08db8b2e6d2df2a61f607a2d
michaelsembwever <mick@semb.wever.org>
2006-07-25 12:22:20 +0000

    warn that appenders will be changed
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3339 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/test/java/no/schibstedsok/front/searchportal/TestCase.java
+++ src/test/java/no/schibstedsok/front/searchportal/TestCase.java
@@ -16,16 +16,19 @@ import org.apache.log4j.MDC;
  * @author <a href="mailto:mick@wever.org">Michael Semb Wever</a>
  */
 public abstract class TestCase extends junit.framework.TestCase {
-    
+
     // Constants -----------------------------------------------------
-    
+
     private static final Logger LOG = Logger.getLogger(TestCase.class);
-    
+
     // Attributes ----------------------------------------------------
-    
+
     // Static --------------------------------------------------------
-    
+
     static{
+    
+        removingAppendersWarning();
+
         // This will be dangerous if this class is loaded in non-test operation
         if(Logger.getRootLogger().getAppender("TEST_APPEND") == null){
             // switch appenders over to the test logfile
@@ -33,20 +36,21 @@ public abstract class TestCase extends junit.framework.TestCase {
             Logger.getRootLogger().addAppender(LOG.getAppender("TEST_APPEND"));
         }
     }
-    
+
     // Constructors --------------------------------------------------
-    
+
     /** Creates a new instance of TestCase */
     public TestCase(final String testName) {
         super(testName);
     }
-    
+
     // Public --------------------------------------------------------
-    
+
     // Z implementation ----------------------------------------------
-    
+
     // no.schibstedsok.front.searchportal.TestCase overrides ----------------------------
-    
+
+    /** TODO comment me. **/
     protected void setUp() throws Exception {
         super.setUp();
 
@@ -55,22 +59,34 @@ public abstract class TestCase extends junit.framework.TestCase {
         
     }
 
+    /** TODO comment me. **/
     protected void tearDown() throws Exception {
         super.tearDown();
-        
+
         MDC.remove("test");
     }    
-    
+
     // Package protected ---------------------------------------------
-    
+
     // Protected -----------------------------------------------------
-    
+
     // Private -------------------------------------------------------
+    
+    private static void removingAppendersWarning(){
+        
+        LOG.warn("==================================");
+        LOG.warn("REMOVING ALL APPENDERS FOR TESTING");
+        LOG.warn("==================================");
+        
+        System.out.println("==================================");
+        System.out.println("REMOVING ALL APPENDERS FOR TESTING");
+        System.out.println("==================================");
+    }
     
     // Inner classes -------------------------------------------------
 
 
-    
+
     
     
 }
