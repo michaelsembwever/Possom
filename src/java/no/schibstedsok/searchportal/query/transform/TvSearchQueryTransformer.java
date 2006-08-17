4436d76521ef58bf4da84d3a4a79e62bfeb3ff55
ajamtli <ajamtli@sesam.no>
2006-08-17 13:30:20 +0000

    SEARCH-836
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3405 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/java/no/schibstedsok/searchportal/query/transform/TvSearchQueryTransformer.java
+++ src/java/no/schibstedsok/searchportal/query/transform/TvSearchQueryTransformer.java
@@ -75,7 +75,7 @@ public final class TvSearchQueryTransformer extends AbstractQueryTransformer {
                 cal.setTimeInMillis(cal.getTimeInMillis() + 1000 * 60 * 60 * 24 * 7);
                 filter.append(new SimpleDateFormat("yyyy-MM-dd'T00:00:00Z'").format(cal.getTime()));
             }
-        } else if (isRSS && parameters.get("nav_categories") != null) {
+        } else if (!navDays && parameters.get("nav_categories") != null) {
             cal.setTimeInMillis(cal.getTimeInMillis() + 1000 * 60 * 60 * 24);
             filter.append(" +starttime:<");
             filter.append(new SimpleDateFormat("yyyy-MM-dd'T03:00:00Z'").format(cal.getTime()));
