1bc047d794d3d533116a2fa6e07b70075b5da27d
ajamtli <ajamtli@sesam.no>
2006-10-04 12:13:00 +0000

    added the div index
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3661 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/tunnel/tunnels.sh
+++ src/tunnel/tunnels.sh
@@ -5,7 +5,7 @@
 # 15100 ---> queryServerURL.1, sesam.se newsQueryServerURL
 # 15200 ---> queryServerURL.2
 # 15400 ---> tokenevaluator, relevantqueries
-# 15500 ---> queryServerURL.4
+# 15500 ---> queryServerURL.4  DIV index
 # 15151 ---> queryServerURL.5
 # 15252 ---> picsearch
 # 25100 ---> sesam.se queryServerURL.1
@@ -17,6 +17,7 @@ ssh \
 -L 15700:10.16.195.250:15100 \
 -L 15200:10.16.195.249:15300 \
 -L 15400:10.16.195.250:15200 \
+-L 15500:10.16.195.250:15400 \
 -L 15100:sch-fast-b01.dev.osl.basefarm.net:15100 \
 -L 15151:81.93.165.134:25100 \
 -L 15252:license.picsearch.com:80  \
