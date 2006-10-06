b4535f578c04aae0c513702cd22f4fc8dda02636
andersb <andersb@sesam.no>
2006-10-06 13:56:31 +0000

    Fixed constructor
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3682 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/java/no/schibstedsok/searchportal/mode/config/AdvancedFastSearchConfiguration.java
+++ src/java/no/schibstedsok/searchportal/mode/config/AdvancedFastSearchConfiguration.java
@@ -4,7 +4,12 @@ package no.schibstedsok.searchportal.mode.config;
  *
  */
 public class AdvancedFastSearchConfiguration extends FastSearchConfiguration {
-    public AdvancedFastSearchConfiguration(final AdvancedFastSearchConfiguration asc){
+	 /** TODO comment me. **/
+    public AdvancedFastSearchConfiguration() {
+        super(null);
+	}
+    
+	public AdvancedFastSearchConfiguration(final SearchConfiguration asc){
         super(asc);
     }
 }
