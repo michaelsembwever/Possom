ab55765f46c9a580c6c061e2a36d82bed71c8a8d
michaelsembwever <mick@semb.wever.org>
2006-10-09 14:32:12 +0000

    SEARCH-626 - Remove svn:external src/test/conf from search-portal project
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3695 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ src/test/stress/bin/start-background-load.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Create background load on bleedingsearch
+#
+#  create jmeter slaves to create proper distribution of requests
+#
+
+cd /www/schibstedsok/home/mickw
+echo "Fetching new copy of bleedingsearch-background-stress.jmx"
+rm bleedingsearch-background-stress.jmx
+wget https://dev.schibstedsok.no/svn/search-config/sesam.no/trunk/war/src/main/conf/bleedingsearch-background-stress.jmx
+echo "Cleaning jmeter.log"
+> jmeter.log
+echo "Backgrounding jmeter in non-gui mode..."
+screen -d -m jmeter/bin/jmeter -n -t bleedingsearch-background-stress.jmx -l jmeter.log
