6aa7f63ffc62738799850e66336a8b6a0b41a54f
magnuse <magnuse@sesam.no>
2006-10-24 14:48:23 +0000

    Added yahoo image search. This is only reachable from sch-http01 at the moment. So i'm tunneling it via a nohup tunnel i've set up on sch-login01.
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3844 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/tunnel/tunnels.sh
+++ src/tunnel/tunnels.sh
@@ -8,6 +8,7 @@
 # 15500 ---> queryServerURL.4  DIV index
 # 15151 ---> queryServerURL.5
 # 15252 ---> picsearch
+# 15253 ---> yahoo image search.
 # 25100 ---> sesam.se queryServerURL.1
 # 25200 ---> sesam.se tokenevaluator
 # 25300 ---> sesam.se fast server (svd2,n24)
@@ -22,6 +23,7 @@ ssh \
 -L 15100:10.16.195.250:15100 \
 -L 15151:sch-fast02.dev.osl.basefarm.net:25100 \
 -L 15252:license.picsearch.com:80  \
+-L 15253:sch-login01.osl.basefarm.net:15253 \
 -L 25100:sch-fast-se-admin01.osl.basefarm.net:15100 \
 -L 25200:sch-fast-query-se.osl.basefarm.net:25200 \
 -L 25300:sch-fast-query-se.osl.basefarm.net:25100 \
