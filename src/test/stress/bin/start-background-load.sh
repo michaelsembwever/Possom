b7604454d0e84e18da4241e6d27f816a5c715ac4
michaelsembwever <mick@semb.wever.org>
2006-11-15 18:33:10 +0000

    stress testing against stress.alpha.test.sesam.no
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@3959 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/test/stress/bin/start-background-load.sh
+++ src/test/stress/bin/start-background-load.sh
@@ -6,10 +6,10 @@
 #
 
 cd /www/schibstedsok/home/mickw
-echo "Fetching new copy of bleedingsearch-background-stress.jmx"
-rm bleedingsearch-background-stress.jmx
-wget https://dev.schibstedsok.no/svn/search-portal/trunk/src/test/stress/conf/bleedingsearch-background-stress.jmx
+echo "Fetching new copy of alpha-background-stress.jmx"
+rm alpha-background-stress.jmx
+wget https://dev.schibstedsok.no/svn/search-portal/trunk/src/test/stress/conf/alpha-background-stress.jmx
 echo "Cleaning jmeter.log"
 > jmeter.log
 echo "Backgrounding jmeter in non-gui mode..."
-screen -d -m jmeter/bin/jmeter -n -t bleedingsearch-background-stress.jmx -l jmeter.log
+screen -d -m jmeter/bin/jmeter -n -t alpha-background-stress.jmx -l jmeter.log
