3a3e8883a416bc59c8f6f1aa36a654a11bbfd423
ssmaeklu <ssmaeklu@sesam.no>
2006-12-01 13:48:06 +0000

    Fix to handle problem with multi valued modifier fields.
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4066 cdc3010f-a60b-0410-91f3-d90f869209c8

--- core-api/src/main/java/no/schibstedsok/searchportal/mode/command/NavigatableESPFastCommand.java
+++ core-api/src/main/java/no/schibstedsok/searchportal/mode/command/NavigatableESPFastCommand.java
@@ -305,8 +305,10 @@ public class NavigatableESPFastCommand extends ESPFastSearchCommand {
             
             while (modifers.hasNext()) {
                 IModifier modifier = (IModifier) modifers.next();
-                Modifier mod = new Modifier(modifier.getName(), modifier.getCount(), nav);
-                searchResult.addModifier(navigatorKey, mod);
+                if (!navigatedValues.containsKey(nav.getField()) || modifier.getName().equals(navigatedValues.get(nav.getField())[0])) {
+                    Modifier mod = new Modifier(modifier.getName(), modifier.getCount(), nav);
+                    searchResult.addModifier(navigatorKey, mod);
+                }
             }
             
             if (searchResult.getModifiers(navigatorKey) != null) {
