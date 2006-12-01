40b2234cd82aa870565476c5e728269a14484c24
ssanjamt <ssanjamt@sesam.no>
2006-12-01 12:56:24 +0000

    directive supports offset
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4064 cdc3010f-a60b-0410-91f3-d90f869209c8

--- core-api/src/main/java/no/schibstedsok/searchportal/view/velocity/WeekdayDirective.java
+++ core-api/src/main/java/no/schibstedsok/searchportal/view/velocity/WeekdayDirective.java
@@ -7,13 +7,11 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.velocity.context.InternalContextAdapter;
 import org.apache.velocity.exception.MethodInvocationException;
 import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.runtime.directive.Directive;
-import org.apache.velocity.runtime.parser.Token;
 import org.apache.velocity.runtime.parser.node.Node;
 
 public class WeekdayDirective extends Directive {
@@ -38,7 +36,7 @@ public class WeekdayDirective extends Directive {
      * {@inheritDoc}
      */
     public boolean render(final InternalContextAdapter context, final Writer writer, final Node node) throws IOException, ResourceNotFoundException, ParseErrorException, MethodInvocationException {
-        if (node.jjtGetNumChildren() != 0) {
+        if (node.jjtGetNumChildren() >  1) {
             rsvc.error("#" + getName() + " - wrong number of arguments");
             return false;
         }
@@ -47,6 +45,11 @@ public class WeekdayDirective extends Directive {
 //            final int daysToAdd = Integer.parseInt(node.jjtGetChild(0).value(context).toString());
             
             Calendar cal = Calendar.getInstance();
+            if (node.jjtGetNumChildren() == 1) {
+                int offset = Integer.parseInt(node.jjtGetChild(0).value(context).toString());
+                cal.add(Calendar.DAY_OF_YEAR, offset);
+            }
+            
             List<HashMap> list = new ArrayList();
             
             for (int i = 0; i < 7; i++) {
