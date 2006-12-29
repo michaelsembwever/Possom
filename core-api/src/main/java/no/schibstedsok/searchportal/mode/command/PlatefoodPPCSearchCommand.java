afe7d1534088442abbe6044a12f7dcf13fcb2bac
ssenrogn <ssenrogn@sesam.no>
2006-12-29 08:50:18 +0000

    Added logging of platefood url.
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4234 cdc3010f-a60b-0410-91f3-d90f869209c8

--- core-api/src/main/java/no/schibstedsok/searchportal/mode/command/PlatefoodPPCSearchCommand.java
+++ core-api/src/main/java/no/schibstedsok/searchportal/mode/command/PlatefoodPPCSearchCommand.java
@@ -136,6 +136,8 @@ public final class PlatefoodPPCSearchCommand extends AbstractYahooSearchCommand 
         }  catch (UnsupportedEncodingException e) {
             throw new InfrastructureException(e);
         }
+
+        LOG.debug("Platefood-url: " + url.toString());
         return url.toString();
     }
 
