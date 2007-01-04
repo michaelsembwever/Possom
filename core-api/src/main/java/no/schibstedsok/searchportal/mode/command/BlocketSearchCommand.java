85d06c1bb38eb33c609fec1a9a7535db76a548ed
ssanbern <ssanbern@sesam.no>
2007-01-04 15:04:11 +0000

    added logger
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4267 cdc3010f-a60b-0410-91f3-d90f869209c8

--- core-api/src/main/java/no/schibstedsok/searchportal/mode/command/BlocketSearchCommand.java
+++ core-api/src/main/java/no/schibstedsok/searchportal/mode/command/BlocketSearchCommand.java
@@ -2,14 +2,17 @@ package no.schibstedsok.searchportal.mode.command;
 
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
-import java.util.Collections;
-import java.util.HashMap;
+
+
 import java.util.Map;
-import java.util.Properties;
+
 
 import javax.xml.rpc.ServiceException;
 import javax.xml.rpc.holders.LongHolder;
 import javax.xml.rpc.holders.StringHolder;
+
+import org.apache.log4j.Logger;
+
 import se.blocket.www2.search.SearchLocator;
 import se.blocket.www2.search.SearchPortType;
 import no.schibstedsok.searchportal.InfrastructureException;
@@ -18,11 +21,15 @@ import no.schibstedsok.searchportal.query.token.TokenEvaluationEngine;
 import no.schibstedsok.searchportal.query.token.TokenPredicate;
 import no.schibstedsok.searchportal.result.BasicSearchResult;
 import no.schibstedsok.searchportal.result.SearchResult;
-import no.schibstedsok.searchportal.site.config.PropertiesLoader;
-import no.schibstedsok.searchportal.site.config.UrlResourceLoader;
+
 
 
 public class BlocketSearchCommand extends AbstractWebServiceSearchCommand {
+	
+	private static final Logger LOG = Logger
+	.getLogger(BlocketSearchCommand.class);
+	private static final String ERR_FAILED_BLOCKET_SEARCH = "Failed Prisjakt search command";
+	
 	
 	
 	public BlocketSearchCommand(final Context cxt,
@@ -35,11 +42,7 @@ public class BlocketSearchCommand extends AbstractWebServiceSearchCommand {
 		
 		final TokenEvaluationEngine engine = context.getTokenEvaluationEngine();
 		
-		final boolean isElectronic = engine.evaluateQuery(TokenPredicate.PRODUCT_ELECTRONIC,context.getQuery());
-		final boolean isHobby = engine.evaluateQuery(TokenPredicate.PRODUCT_HOBBY,context.getQuery());
-		
-		
-		
+		final boolean isBlocket = engine.evaluateQuery(TokenPredicate.BLOCKET,context.getQuery());
 		
 		BlocketSearchConfiguration bsc = (BlocketSearchConfiguration) context.getSearchConfiguration();
 		
@@ -48,45 +51,50 @@ public class BlocketSearchCommand extends AbstractWebServiceSearchCommand {
 		final SearchLocator service = new SearchLocator();
 		
 		final SearchResult result = new BasicSearchResult(this); 
-		
-		try 
+		if(isBlocket)
 		{
-			final SearchPortType port = service.getsearchPort(new java.net.URL(service.getsearchPortAddress()));
-			String query = getTransformedQuery();
-		
-			LongHolder lholder = new LongHolder();
-			StringHolder sholder = new StringHolder();
-			
-			String categoryIndex =(String) m.get(query);
-			if(categoryIndex!=null)
+			try 
 			{
-				port.search(query, Integer.parseInt(categoryIndex), lholder, sholder);
+				final SearchPortType port = service.getsearchPort(new java.net.URL(service.getsearchPortAddress()));
+				String query = getTransformedQuery();
+			
+				LongHolder lholder = new LongHolder();
+				StringHolder sholder = new StringHolder();
+				
+				String categoryIndex =(String) m.get(query);
+				if(categoryIndex!=null)
+				{
+					port.search(query, Integer.parseInt(categoryIndex), lholder, sholder);
+				}
+				
+				String nads = Long.toString(lholder.value);
+				result.addField("searchquery", query);
+				result.addField("numberofads", nads);
+				result.addField("blocketbackurl", sholder.value);
+				
+				result.setHitCount(1);
+				
+				
+		
+			}
+			catch (ServiceException se) 
+			{
+				LOG.error(ERR_FAILED_BLOCKET_SEARCH, se);
+				throw new InfrastructureException(se);
+			} 
+			catch (MalformedURLException murle) 
+			{
+				LOG.error(ERR_FAILED_BLOCKET_SEARCH, murle);
+				throw new InfrastructureException(murle);
+			} 
+			catch (RemoteException re) 
+			{
+				LOG.error(ERR_FAILED_BLOCKET_SEARCH, re);
+				throw new InfrastructureException(re);
 			}
 			
-			String nads = Long.toString(lholder.value);
-			result.addField("searchquery", query);
-			result.addField("numberofads", nads);
-			result.addField("blocketbackurl", sholder.value);
-			
-			result.setHitCount(1);
-			
-			
-	
-		}
-		catch (ServiceException se) 
-		{
-			throw new InfrastructureException(se);
-		} 
-		catch (MalformedURLException murle) 
-		{
-			throw new InfrastructureException(murle);
-		} 
-		catch (RemoteException re) 
-		{
-			throw new InfrastructureException(re);
 		}
 		
-		//return null;
 		return result;
 		
 	}
