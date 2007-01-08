9da57118a9e7cbb77bdbd08b3e5fdec1c366654d
ssanbern <ssanbern@sesam.no>
2007-01-08 10:31:32 +0000

    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4275 cdc3010f-a60b-0410-91f3-d90f869209c8

--- core-api/src/main/java/no/schibstedsok/searchportal/mode/command/BlocketSearchCommand.java
+++ core-api/src/main/java/no/schibstedsok/searchportal/mode/command/BlocketSearchCommand.java
@@ -3,9 +3,7 @@ package no.schibstedsok.searchportal.mode.command;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 
-
 import java.util.Map;
-
 
 import javax.xml.rpc.ServiceException;
 import javax.xml.rpc.holders.LongHolder;
@@ -23,81 +21,72 @@ import no.schibstedsok.searchportal.query.token.TokenPredicate;
 import no.schibstedsok.searchportal.result.BasicSearchResult;
 import no.schibstedsok.searchportal.result.SearchResult;
 
-
-
 public class BlocketSearchCommand extends AbstractWebServiceSearchCommand {
-	
+
 	private static final Logger LOG = Logger
-	.getLogger(BlocketSearchCommand.class);
+			.getLogger(BlocketSearchCommand.class);
+
 	private static final String ERR_FAILED_BLOCKET_SEARCH = "Failed Prisjakt search command";
-	
-	
-	
+
 	public BlocketSearchCommand(final Context cxt,
 			final Map<String, Object> parameters) {
-		super(cxt, parameters);}
+		super(cxt, parameters);
+	}
 
 	@Override
-	public SearchResult execute() 
-	{
-		
+	public SearchResult execute() {
+
 		final TokenEvaluationEngine engine = context.getTokenEvaluationEngine();
-		
-		final boolean isBlocket = engine.evaluateQuery(TokenPredicate.BLOCKET,context.getQuery());
-		
-		BlocketSearchConfiguration bsc = (BlocketSearchConfiguration) context.getSearchConfiguration();
-		
+
+		final boolean isBlocket = engine.evaluateQuery(TokenPredicate.BLOCKET,
+				context.getQuery());
+
+		BlocketSearchConfiguration bsc = (BlocketSearchConfiguration) context
+				.getSearchConfiguration();
+
 		Map m = bsc.getBlocketMap();
-		
+
 		final SearchLocator service = new SearchLocator();
-		
-		final SearchResult result = new BasicSearchResult(this); 
-		if(isBlocket)
-		{
-			try 
-			{
-				final SearchPortType port = service.getsearchPort(new java.net.URL(service.getsearchPortAddress()));
+
+		final SearchResult result = new BasicSearchResult(this);
+		if (isBlocket) {
+			try {
+				final SearchPortType port = service
+						.getsearchPort(new java.net.URL(service
+								.getsearchPortAddress()));
 				String query = getTransformedQuery();
-			
+
 				LongHolder lholder = new LongHolder();
 				StringHolder sholder = new StringHolder();
 				String trimQ = StringUtils.deleteWhitespace(query);
-				String categoryIndex =(String) m.get(trimQ);
-				if(categoryIndex!=null)
-				{
-					port.search(query, Integer.parseInt(categoryIndex), lholder, sholder);
+				String categoryIndex = (String) m.get(trimQ);
+				if (categoryIndex != null) {
+					port.search(query, Integer.parseInt(categoryIndex),
+							lholder, sholder);
 				}
-				
+
 				String nads = Long.toString(lholder.value);
 				result.addField("searchquery", query);
 				result.addField("numberofads", nads);
 				result.addField("blocketbackurl", sholder.value);
-				
+
 				result.setHitCount(1);
-				
-				
-		
-			}
-			catch (ServiceException se) 
-			{
+
+			} catch (ServiceException se) {
 				LOG.error(ERR_FAILED_BLOCKET_SEARCH, se);
 				throw new InfrastructureException(se);
-			} 
-			catch (MalformedURLException murle) 
-			{
+			} catch (MalformedURLException murle) {
 				LOG.error(ERR_FAILED_BLOCKET_SEARCH, murle);
 				throw new InfrastructureException(murle);
-			} 
-			catch (RemoteException re) 
-			{
+			} catch (RemoteException re) {
 				LOG.error(ERR_FAILED_BLOCKET_SEARCH, re);
 				throw new InfrastructureException(re);
 			}
-			
+
 		}
-		
+
 		return result;
-		
+
 	}
 
 }
