a5b2cf7203d58d00ec12689697cd984f08dc788c
michaelsembwever <mick@semb.wever.org>
2007-01-17 08:59:53 +0000

    parametersed to run for either alpha or beta
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4338 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/test/stress/bin/start-background-load.sh
+++ src/test/stress/bin/start-background-load.sh
@@ -5,11 +5,16 @@
 #  create jmeter slaves to create proper distribution of requests
 #
 
-cd /www/schibstedsok/home/mickw
-echo "Fetching new copy of alpha-background-stress.jmx"
-rm alpha-background-stress.jmx
+if [[ "x" == "x$1" ]] ; then
+    echo "Usage: ./start-background-load.sh (alpha|beta)"
+    exit 1
+fi
+
+echo "Fetching new copy of $1-background-stress.jmx"
+rm -f alpha-background-stress.jmx
 wget https://dev.schibstedsok.no/svn/search-portal/trunk/src/test/stress/conf/alpha-background-stress.jmx
+cat alpha-background-stress.jmx | sed "s/alpha/beta\/$1/" > beta-background-stress.jmx
 echo "Cleaning jmeter.log"
 > jmeter.log
 echo "Backgrounding jmeter in non-gui mode..."
-screen -d -m jmeter/bin/jmeter -n -t alpha-background-stress.jmx -l jmeter.log
+screen -d -m jmeter/bin/jmeter -n -t $1-background-stress.jmx -l jmeter.log
