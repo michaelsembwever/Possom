dc4fe61a745bafcfc8457e161a892e7af48a7795
daniele <daniele@sesam.no>
2007-01-24 07:50:23 +0000

    AND between search terms instead of OR in ad-search
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4368 cdc3010f-a60b-0410-91f3-d90f869209c8

--- core-api/src/main/java/no/schibstedsok/searchportal/mode/command/CatalogueAdsSearchCommand.java
+++ core-api/src/main/java/no/schibstedsok/searchportal/mode/command/CatalogueAdsSearchCommand.java
@@ -60,14 +60,7 @@ public class CatalogueAdsSearchCommand extends AdvancedFastSearchCommand {
      */
     protected void visitImpl(final LeafClause clause) {
     	
-    	
-//    	subQuery1 += (subQuery1.length()>0?QL_AND:" ") + " iypcfspkeywords1:"+clause.getTerm()+queryTwo;
-//    	subQuery2 += (subQuery2.length()>0?QL_AND:" ") + " iypcfspkeywords2:"+clause.getTerm()+queryTwo;
-//    	subQuery3 += (subQuery3.length()>0?QL_AND:" ") + " iypcfspkeywords3:"+clause.getTerm()+queryTwo;
-//    	subQuery4 += (subQuery4.length()>0?QL_AND:" ") + " iypcfspkeywords4:"+clause.getTerm()+queryTwo;
-//    	subQuery5 += (subQuery5.length()>0?QL_AND:" ") + " iypcfspkeywords5:"+clause.getTerm()+queryTwo;    	
-    	
-    	String term = clause.getTerm();
+    	String term = getTransformedTerm(clause);
     	appendToQueryRepresentation("(");
     	appendToQueryRepresentation("( (iypcfspkeywords5:"+term+queryTwo+") OR ((iypcfspkeywords5:"+term+"ingensteds AND iypspgep5:ingensteds) ANDNOT (iypcfspkeywords5:"+term+queryTwo+"))) OR"); 
     	appendToQueryRepresentation("( (iypcfspkeywords4:"+term+queryTwo+") OR ((iypcfspkeywords4:"+term+"ingensteds AND iypspgeo4:ingensteds) ANDNOT (iypcfspkeywords4:"+term+queryTwo+"))) OR"); 
@@ -76,36 +69,4 @@ public class CatalogueAdsSearchCommand extends AdvancedFastSearchCommand {
 		appendToQueryRepresentation("( (iypcfspkeywords1:"+term+queryTwo+") OR ((iypcfspkeywords1:"+term+"ingensteds AND iypspgeo1:ingensteds) ANDNOT (iypcfspkeywords1:"+term+queryTwo+")))");
     	appendToQueryRepresentation(")");
     }
-    
-    /**
-     * {@inheritDoc}
-     */
-    protected void visitImpl(final DefaultOperatorClause clause) {
-    	LOG.info("visitImpl DefaultOperatorClause");
-        clause.getFirstClause().accept(this);
-
-        final boolean hasEmptyLeaf = isEmptyLeaf(clause.getFirstClause()) || isEmptyLeaf(clause.getSecondClause());
-
-        if (!(hasEmptyLeaf || clause.getSecondClause() instanceof NotClause)) {
-            appendToQueryRepresentation("OR");
-        }
-
-        clause.getSecondClause().accept(this);    	
-    }
-    
-    /**
-     * Returns true iff the clause is a leaf clause and if it will not produce any output in the query representation.
-     *
-     * @param clause The clause to examine.
-     *
-     * @return true iff leaf is empty.
-     */
-    private boolean isEmptyLeaf(final Clause clause) {
-        if (clause instanceof LeafClause) {
-            final LeafClause leafClause = (LeafClause) clause;
-            return getFieldFilter(leafClause) != null || getTransformedTerm(clause).equals("");
-        } else {
-            return false;
-        }
-    }    
 }
