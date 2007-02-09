adfd6334dee55a4020689d56a2c9e6db0f764b3f
ssanbern <ssanbern@sesam.no>
2007-02-09 16:07:05 +0000

    add hittamap
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4428 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ core-api/src/main/java/no/schibstedsok/searchportal/mode/config/HittaMapSearchConfiguration.java
@@ -0,0 +1,19 @@
+package no.schibstedsok.searchportal.mode.config;
+
+/**
+ * Configuration for the map command.
+ */
+public class HittaMapSearchConfiguration extends AbstractSearchConfiguration {
+    
+	
+	/**
+     * Creates a new instance of this search configuration.
+     */
+    public HittaMapSearchConfiguration(final SearchConfiguration sc) {
+        super(sc);
+    
+    }
+    
+    
+}
+    
