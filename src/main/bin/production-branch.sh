2d2a8c27ae153c780305bf95a681c5c46088ad03
michaelsembwever <mick@semb.wever.org>
2007-02-18 14:31:44 +0000

    moving deployment scripts to search-main stack
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4454 cdc3010f-a60b-0410-91f3-d90f869209c8

--- src/main/bin/production-branch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#
-# Version to use in production (& beta)
-#  Used by update.sh beta & update.sh production
-#
-
-VERSION=2.8-SNAPSHOT
-export VERSION
--- src/main/bin/test-branch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#
-# Version to use in production (& searchdev)
-#  Used by update-searchdev.sh & update-production.sh
-#
-
-VERSION=2.9-SNAPSHOT
-export VERSION
