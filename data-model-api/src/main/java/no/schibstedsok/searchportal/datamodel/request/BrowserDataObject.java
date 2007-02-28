d5aa80ecf4f7beedcf0b9cc71b835ec30eaa12eb
michaelsembwever <mick@semb.wever.org>
2007-02-28 19:52:41 +0000

    adding user-agent
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4527 cdc3010f-a60b-0410-91f3-d90f869209c8

--- data-model-api/src/main/java/no/schibstedsok/searchportal/datamodel/request/BrowserDataObject.java
+++ data-model-api/src/main/java/no/schibstedsok/searchportal/datamodel/request/BrowserDataObject.java
@@ -11,14 +11,17 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Locale;
 import no.schibstedsok.searchportal.datamodel.generic.DataObject;
+import no.schibstedsok.searchportal.datamodel.generic.StringDataObject;
 
-/**
+/** Holds information regarding the user's browser.
+ * Typically HTTP Request Headers.
  *
  * @author <a href="mailto:mick@semb.wever.org">Mck</a>
  * @version <tt>$Id$</tt>
  */
 @DataObject
 public interface BrowserDataObject extends Serializable {
+    StringDataObject getUserAgent();
     Locale getLocale();
     List<Locale> getSupportedLocales();
 }
