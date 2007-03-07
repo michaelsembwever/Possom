df1ed6e3967253f92a45263e803218460105c932
michaelsembwever <mick@semb.wever.org>
2007-03-07 20:36:56 +0000

    doco
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4549 cdc3010f-a60b-0410-91f3-d90f869209c8

--- data-model-api/src/main/java/no/schibstedsok/searchportal/datamodel/query/QueryDataObject.java
+++ data-model-api/src/main/java/no/schibstedsok/searchportal/datamodel/query/QueryDataObject.java
@@ -1,5 +1,5 @@
-// Copyright (2007) Schibsted Søk AS
-/*
+/* Copyright (2007) Schibsted Søk AS
+ *
  * QueryDataObject.java
  *
  * Created on 23 January 2007, 12:42
@@ -15,7 +15,8 @@ import no.schibstedsok.searchportal.datamodel.access.AccessAllow;
 import no.schibstedsok.searchportal.datamodel.access.AccessDisallow;
 import no.schibstedsok.searchportal.query.Query;
 
-/**
+/** The QueryDataObject is the datamodel's container around the user inputted query string 
+ * and the same query parsed into a Query tree.
  *
  * @author <a href="mailto:mick@semb.wever.org">Mck</a>
  * @version <tt>$Id$</tt>
