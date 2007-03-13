5dd6ed543ca577fcd034ca141854653b506d2bf8
ssanjamt <ssanjamt@sesam.no>
2007-03-13 12:26:29 +0000

    header for xml output
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4567 cdc3010f-a60b-0410-91f3-d90f869209c8

--- war/src/main/java/no/schibstedsok/searchportal/http/servlet/SearchServlet.java
+++ war/src/main/java/no/schibstedsok/searchportal/http/servlet/SearchServlet.java
@@ -239,39 +239,39 @@ public final class SearchServlet extends HttpServlet {
             } catch (IOException ex) {
                 LOG.error(ex.getMessage(), ex);
             }
-        } else if (request.getParameter("output") != null && request.getParameter("output").equals("savedecorator")) {
-                String showid = request.getParameter("showId");
-                String userAgent = request.getHeader("User-Agent");
-                String fileName = ".vcs";
-                String charset = "utf-8";
-                if(userAgent.indexOf("Windows") != -1){
-                	charset = "iso-8859-1";
-                }else if(userAgent.indexOf("Mac OS X") != -1)
-                   fileName=".ics";
-                if(showid == null)
-                   showid="";
-                response.setContentType("text/calendar; charset=" +charset);
-                response.setHeader("Content-Disposition","attachment;filename=sesam-tvsok-" +showid +fileName	);
-
-        } else if (request.getParameter("output") != null && request.getParameter("output").equals("vcarddecorator")) {
-                String showid = request.getParameter("showId");
-                String userAgent = request.getHeader("User-Agent");
-                String fileName = ".vcf";
-                String charset = "utf-8";
-                if(userAgent.indexOf("Windows") != -1)
-                    charset = "iso-8859-1";
-                if(showid == null)
-                   showid="";
-                response.setCharacterEncoding(charset);
-                response.setContentType("text/x-vcard; charset=" +charset);
-                response.setHeader("Content-Disposition","attachment;filename=vcard-" +showid + ".vcf");
-        } else if (request.getParameter("output") != null && request.getParameter("output").equals("opensearch")) {
-                String charset = "utf-8";
-                response.setCharacterEncoding(charset);
-                response.setContentType("text/xml; charset=utf-8");
+        } else if (request.getParameter ("output") != null && request.getParameter ("output").equals ("savedecorator")) {
+            String showid = request.getParameter ("showId");
+            String userAgent = request.getHeader ("User-Agent");
+            String fileName = ".vcs";
+            String charset = "utf-8";
+            if(userAgent.indexOf ("Windows") != -1){
+                charset = "iso-8859-1";
+            }else if(userAgent.indexOf ("Mac OS X") != -1)
+                fileName=".ics";
+            if(showid == null)
+                showid="";
+            response.setContentType ("text/calendar; charset=" +charset);
+            response.setHeader ("Content-Disposition","attachment;filename=sesam-tvsok-" +showid +fileName	);
+            
+        } else if (request.getParameter ("output") != null && request.getParameter ("output").equals ("vcarddecorator")) {
+            String showid = request.getParameter ("showId");
+            String userAgent = request.getHeader ("User-Agent");
+            String fileName = ".vcf";
+            String charset = "utf-8";
+            if(userAgent.indexOf ("Windows") != -1)
+                charset = "iso-8859-1";
+            if(showid == null)
+                showid="";
+            response.setCharacterEncoding (charset);
+            response.setContentType ("text/x-vcard; charset=" +charset);
+            response.setHeader ("Content-Disposition","attachment;filename=vcard-" +showid + ".vcf");
+        } else if (request.getParameter ("output") != null
+                && (request.getParameter ("output").equals ("opensearch") || request.getParameter ("output").equals ("xml"))) {
+            String charset = "utf-8";
+            response.setCharacterEncoding (charset);
+            response.setContentType ("text/xml; charset=utf-8");
         } else {
-
-            response.setContentType("text/html; charset=utf-8");
+            response.setContentType ("text/html; charset=utf-8");
         }
     }
 
