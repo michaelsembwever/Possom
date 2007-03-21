51bb5ed96346ae906e0d449f0312fc90006d3618
gttersen <gttersen@sesam.no>
2007-03-21 10:47:18 +0000

    Need access to the datamodel in the QueryTransformer. Looks like this is the way to do it.
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4596 cdc3010f-a60b-0410-91f3-d90f869209c8

--- query-transform-spi/src/main/java/no/schibstedsok/searchportal/query/transform/QueryTransformer.java
+++ query-transform-spi/src/main/java/no/schibstedsok/searchportal/query/transform/QueryTransformer.java
@@ -2,16 +2,18 @@
 package no.schibstedsok.searchportal.query.transform;
 
 
-import java.util.Map;
-import no.schibstedsok.searchportal.site.config.ResourceContext;
+import no.schibstedsok.searchportal.datamodel.DataModelContext;
 import no.schibstedsok.searchportal.query.Clause;
+import no.schibstedsok.searchportal.query.LeafClause;
 import no.schibstedsok.searchportal.query.QueryContext;
 import no.schibstedsok.searchportal.query.Visitor;
 import no.schibstedsok.searchportal.query.XorClause;
-import no.schibstedsok.searchportal.query.LeafClause;
 import no.schibstedsok.searchportal.query.token.TokenEvaluationEngine;
 import no.schibstedsok.searchportal.site.SiteContext;
+import no.schibstedsok.searchportal.site.config.ResourceContext;
 import org.w3c.dom.Element;
+
+import java.util.Map;
 
 
 /**
@@ -21,48 +23,56 @@ import org.w3c.dom.Element;
  * @author <a href="mailto:magnus.eklund@schibsted.no">Magnus Eklund</a>
  * @version <tt>$Revision: 3829 $</tt>
  */
-public interface QueryTransformer extends Visitor, Cloneable{
+public interface QueryTransformer extends Visitor, Cloneable {
 
-    public interface Context extends QueryContext, ResourceContext, SiteContext {
+    public interface Context extends QueryContext, ResourceContext, SiteContext, DataModelContext {
 
         /**
          * @deprecated use getTransformedTerms() instead
-         **/
+         */
         String getTransformedQuery();
-        /** Get the terms with their current transformed representations.
-         *
-         **/
-        Map<Clause,String> getTransformedTerms();
 
-        /** For evaluation acitions on individual (or the whole query) terms.
-         **/
+        /**
+         * Get the terms with their current transformed representations.
+         */
+        Map<Clause, String> getTransformedTerms();
+
+        /**
+         * For evaluation acitions on individual (or the whole query) terms.
+         */
         TokenEvaluationEngine getTokenEvaluationEngine();
-        
-        /** QueryTransformers must follow the same XorClause hints as the search command. **/
+
+        /**
+         * QueryTransformers must follow the same XorClause hints as the search command. *
+         */
         void visitXorClause(Visitor visitor, XorClause clause);
 
-        /** QueryTransformers needs information about supported field filters. **/
+        /**
+         * QueryTransformers needs information about supported field filters. *
+         */
         String getFieldFilter(LeafClause clause);
     }
 
-    /** TODO comment me. **/
+    /**
+     * TODO comment me. *
+     */
     void setContext(final Context cxt);
 
     /**
      * Add keywords to query to get better searchresults
      *
-     * @deprecated use the visitor pattern instead via visit(Object)
      * @param originalQuery
      * @return
+     * @deprecated use the visitor pattern instead via visit(Object)
      */
     String getTransformedQuery();
 
     /**
      * Set filter for this query. By analizing the query we can
      * set dynamic filter. For example a special sort order or specify contentsource
-     *
+     * <p/>
      * Example to add docdatetime argument
-     *
+     * <p/>
      * +docdatetime:>2005-10-28
      *
      * @return filterstring
@@ -72,17 +82,19 @@ public interface QueryTransformer extends Visitor, Cloneable{
     /**
      * Set filter for this query. By analizing the query we can
      * set dynamic filter. For example a special sort order or specify contentsource
-     *
+     * <p/>
      * Example to add docdatetime argument
-     *
+     * <p/>
      * +docdatetime:>2005-10-28
      *
      * @return filterstring
      */
     String getFilter();
 
-    /** Force public implementation of Clonable. **/
-    Object clone() throws CloneNotSupportedException ;
-    
+    /**
+     * Force public implementation of Clonable. *
+     */
+    Object clone() throws CloneNotSupportedException;
+
     QueryTransformer readQueryTransformer(final Element element);
 }
