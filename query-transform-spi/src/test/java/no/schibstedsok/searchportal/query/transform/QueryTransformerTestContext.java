5165d4d18fe6a954b39e2cfb1cfbdc7ba9f48d14
gttersen <gttersen@sesam.no>
2007-03-21 14:39:38 +0000

    Fix to implement updated verion of QueryTransformer.Context(). Tests now run.
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4600 cdc3010f-a60b-0410-91f3-d90f869209c8

--- query-transform-spi/src/test/java/no/schibstedsok/searchportal/query/transform/QueryTransformerTestContext.java
+++ query-transform-spi/src/test/java/no/schibstedsok/searchportal/query/transform/QueryTransformerTestContext.java
@@ -11,45 +11,47 @@
 package no.schibstedsok.searchportal.query.transform;
 
 
-import java.util.Map;
-import java.util.Properties;
-import javax.xml.parsers.DocumentBuilder;
-import no.schibstedsok.searchportal.site.SiteTestCase;
+import no.schibstedsok.searchportal.datamodel.DataModel;
+import no.schibstedsok.searchportal.query.Clause;
+import no.schibstedsok.searchportal.query.LeafClause;
+import no.schibstedsok.searchportal.query.Query;
 import no.schibstedsok.searchportal.query.Visitor;
 import no.schibstedsok.searchportal.query.XorClause;
+import no.schibstedsok.searchportal.query.token.TokenEvaluationEngine;
+import no.schibstedsok.searchportal.query.token.TokenEvaluationEngineImpl;
+import no.schibstedsok.searchportal.query.token.TokenEvaluationEngineTestContext;
+import no.schibstedsok.searchportal.site.Site;
+import no.schibstedsok.searchportal.site.SiteContext;
+import no.schibstedsok.searchportal.site.SiteTestCase;
 import no.schibstedsok.searchportal.site.config.DocumentLoader;
 import no.schibstedsok.searchportal.site.config.FileResourceLoader;
 import no.schibstedsok.searchportal.site.config.PropertiesLoader;
-import no.schibstedsok.searchportal.query.Clause;
-import no.schibstedsok.searchportal.query.Query;
-import no.schibstedsok.searchportal.query.LeafClause;
-import no.schibstedsok.searchportal.query.token.TokenEvaluationEngineTestContext;
-import no.schibstedsok.searchportal.query.token.TokenEvaluationEngine;
-import no.schibstedsok.searchportal.query.token.TokenEvaluationEngineImpl;
-import no.schibstedsok.searchportal.site.Site;
-import no.schibstedsok.searchportal.site.SiteContext;
+
+import javax.xml.parsers.DocumentBuilder;
+import java.util.Map;
+import java.util.Properties;
 
 /**
- *
  * @author mick
  */
-public final class QueryTransformerTestContext extends SiteTestCase implements QueryTransformer.Context{
+public final class QueryTransformerTestContext extends SiteTestCase implements QueryTransformer.Context {
 
     private final Query query;
-    private final Map<Clause,String> transformedTerms;
+    private final Map<Clause, String> transformedTerms;
     private final TokenEvaluationEngine tokenEvaluationEngine;
+    private DataModel dataModel;
 
     /**
      * Creates a new instance of TestQueryTransformerContextProvider
      */
     public QueryTransformerTestContext(
             final Query q,
-            final Map<Clause,String> t){
+            final Map<Clause, String> t) {
 
         query = q;
         transformedTerms = t;
-        tokenEvaluationEngine  = new TokenEvaluationEngineImpl(
-            new TokenEvaluationEngineTestContext(query.getQueryString()));
+        tokenEvaluationEngine = new TokenEvaluationEngineImpl(
+                new TokenEvaluationEngineTestContext(query.getQueryString()));
     }
 
     /**
@@ -57,8 +59,8 @@ public final class QueryTransformerTestContext extends SiteTestCase implements Q
      */
     public QueryTransformerTestContext(
             final Query q,
-            final Map<Clause,String> t,
-            final TokenEvaluationEngine tokenEvaluationEngine){
+            final Map<Clause, String> t,
+            final TokenEvaluationEngine tokenEvaluationEngine) {
 
         query = q;
         transformedTerms = t;
@@ -66,48 +68,68 @@ public final class QueryTransformerTestContext extends SiteTestCase implements Q
     }
 
 
-    /** TODO comment me. **/
-    public Map<Clause,String> getTransformedTerms() {
+    /**
+     * TODO comment me. *
+     */
+    public Map<Clause, String> getTransformedTerms() {
         return transformedTerms;
     }
-    /** TODO comment me. **/
+
+    /**
+     * TODO comment me. *
+     */
     public Site getSite() {
         return getTestingSite();
     }
 
-    /** TODO comment me. **/
+    /**
+     * TODO comment me. *
+     */
     public Query getQuery() {
         return query;
     }
-    /** TODO comment me. **/
+
+    /**
+     * TODO comment me. *
+     */
     public String getTransformedQuery() {
         return query.getQueryString();
     }
+
+    public DataModel getDataModel() {
+        return dataModel;
+    }
+
     public PropertiesLoader newPropertiesLoader(
-            final SiteContext siteCxt, 
-            final String resource, 
+            final SiteContext siteCxt,
+            final String resource,
             final Properties properties) {
 
         return FileResourceLoader.newPropertiesLoader(siteCxt, resource, properties);
     }
+
     public DocumentLoader newDocumentLoader(
-            final SiteContext siteCxt, 
-            final String resource, 
+            final SiteContext siteCxt,
+            final String resource,
             final DocumentBuilder builder) {
 
         return FileResourceLoader.newDocumentLoader(siteCxt, resource, builder);
     }
-    /** TODO comment me. **/
-    public TokenEvaluationEngine getTokenEvaluationEngine(){
+
+    /**
+     * TODO comment me. *
+     */
+    public TokenEvaluationEngine getTokenEvaluationEngine() {
         return tokenEvaluationEngine;
     }
 
     public void visitXorClause(Visitor visitor, XorClause clause) {
-       clause.getFirstClause().accept(visitor);
+        clause.getFirstClause().accept(visitor);
     }
 
     public String getFieldFilter(final LeafClause clause) {
         // TODO. Return some thing as AbstractSearchCommand.getFieldFilter
         return null;
     }
+
 }
