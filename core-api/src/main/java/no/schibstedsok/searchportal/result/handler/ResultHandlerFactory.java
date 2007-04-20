3aa42062712edfc612c26ae8a0ea309c0be5f8d6
michaelsembwever <mick@semb.wever.org>
2007-04-20 13:38:24 +0000

    dawdle duckling to  SEARCH-1745 - Configuration classes should be self-responsible for deserialisation
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4845 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ core-api/src/main/java/no/schibstedsok/searchportal/result/handler/ResultHandlerFactory.java
@@ -0,0 +1,84 @@
+/*
+ * ResultHandlerFactory.java
+ *
+ * Created on 26 March 2007, 17:29
+ *
+ */
+
+package no.schibstedsok.searchportal.result.handler;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import no.schibstedsok.searchportal.result.handler.AbstractResultHandlerConfig.Controller;
+
+/** Obtain a working ResultHandler from a given ResultHandlerConfig.
+ *
+ * @author <a href="mailto:mick@semb.wever.org">Mck</a>
+ * @version <tt>$Id$</tt>
+ */
+public final class ResultHandlerFactory {
+    
+    // Constants -----------------------------------------------------
+    
+    
+    // Attributes ----------------------------------------------------
+    
+    // Static --------------------------------------------------------
+    
+    
+    // Constructors --------------------------------------------------
+    
+    /** Creates a new instance of QueryTransformerFactory */
+    private ResultHandlerFactory() {
+    }
+    
+    // Public --------------------------------------------------------
+    
+    // Public --------------------------------------------------------
+    
+    // Public --------------------------------------------------------
+    
+    // Public --------------------------------------------------------
+    
+    /**
+     * 
+     * @param config 
+     * @return 
+     */
+    public static ResultHandler getController(final ResultHandlerConfig config){
+        
+        final String name = "no.schibstedsok.searchportal.result.handler." 
+                + config.getClass().getAnnotation(Controller.class).value();
+        
+        try{
+            
+            final Class<? extends ResultHandler> cls 
+                    = (Class<? extends ResultHandler>)config.getClass().getClassLoader().loadClass(name);
+            
+            final Constructor<? extends ResultHandler> constructor 
+                    = cls.getConstructor(ResultHandlerConfig.class);
+
+            return constructor.newInstance(config);
+            
+        } catch (ClassNotFoundException ex) {
+            throw new IllegalArgumentException(ex);
+        } catch (NoSuchMethodException ex) {
+            throw new IllegalArgumentException(ex);
+        } catch (InvocationTargetException ex) {
+            throw new IllegalArgumentException(ex);
+        } catch (InstantiationException ex) {
+            throw new IllegalArgumentException(ex);
+        } catch (IllegalAccessException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+    
+    // Package protected ---------------------------------------------
+    
+    // Protected -----------------------------------------------------
+    
+    // Private -------------------------------------------------------
+    
+    // Inner classes -------------------------------------------------
+    
+}
