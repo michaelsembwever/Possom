733d927a410d5986e06e6cbcc6fc521dd79ce9a4
michaelsembwever <mick@semb.wever.org>
2007-04-24 09:49:39 +0000

    SEARCH-2010 - Publishing system timeouts since 2.10 release  HTTPClient now tracks statistics of connections it makes.
    git-svn-id: http://sesat.no/svn/sesat-kernel/branches/2.11@4857 cdc3010f-a60b-0410-91f3-d90f869209c8

--- httpclient-api/src/main/java/no/schibstedsok/searchportal/http/HTTPClient.java
+++ httpclient-api/src/main/java/no/schibstedsok/searchportal/http/HTTPClient.java
@@ -14,108 +14,280 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.log4j.Logger;
 
 /** Utility class to fetch URLs and return them as either BufferedReaders or XML documents.
  * Original implementation used Commons HttpClient but keepalive was disabled, due to a FAST Query-Matching bug,
  *  which was its original benefit.
- * The current implementation just using URL and URLConnection directly synchronously.
- * 
+ * The current implementation just using URL and URLConnection directly within the current thread.
+ *
  * Supports protocols http, https, ftp, and file.
  * If no protocol is specified in the host it defaults to http.
- * 
+ *
  * @author <a href="mailto:magnus.eklund@schibsted.no">Magnus Eklund</a>
  * @author <a href="mailto:mick@sesam.no">Mck</a>
  * @version <tt>$Id$</tt>
  */
 public final class HTTPClient {
-    
+
     // Constants -----------------------------------------------------
+
     private static final Logger LOG = Logger.getLogger(HTTPClient.class);
     private static final String DEBUG_USING_URL = "Using url ";
     private static final String DEBUG_USING_HOSTHEADER = "Using host header: ";
-    
+
+    private static final Map<String,Statistic> STATISTICS = new ConcurrentHashMap<String,Statistic>();
+
+    private final String id;
     private final String host, hostHeader;
     private final int port;
-    
+
     private HTTPClient(final String host, final int port, final String hostHeader) {
-        
+
         this.host= host;
         this.port = port;
         this.hostHeader = hostHeader;
+
+        String id = host + ':' + port;
+        if(id.contains("://")){
+            id = id.substring(id.indexOf("://") + 3);
+        }
+        this.id = id;
+        
+        if(null==STATISTICS.get(id)){
+            STATISTICS.put(id, new Statistic(id));
+        }
     }
 
+    /**
+     *
+     * @param id
+     * @param host
+     * @param port
+     * @return
+     */
     public static HTTPClient instance(final String id, final String host, final int port) {
-        
+
         return new HTTPClient(host, port, host);
     }
-    
+
+    /**
+     *
+     * @param id
+     * @param host
+     * @param port
+     * @param hostHeader
+     * @return
+     */
     public static HTTPClient instance(final String id, final String host, final int port, final String hostHeader) {
-        
+
         return new HTTPClient(host, port, hostHeader);
     }
-       
+
     private URL getURL(final String path) throws MalformedURLException{
-        
+
         if( port == 0 ){
             throw new MalformedURLException("Null port");
         }
-        
-        final boolean hasProtocol = host.startsWith("http://") || host.startsWith("https://") 
+
+        final boolean hasProtocol = host.startsWith("http://") || host.startsWith("https://")
                 || host.startsWith("ftp://") || host.startsWith("file://");
                 //host.matches("^(http|https|ftp|file)://");
-        
+
         final URL url = new URL(
                 (hasProtocol ? "" : "http://")
-                + host + ':' + port 
+                + host + ':' + port
                 + path);
-        
+
         LOG.trace(DEBUG_USING_URL + url);
-        
+
         return url;
     }
 
+    /**
+     *
+     * @param id
+     * @param path
+     * @return
+     * @throws java.io.IOException
+     * @throws org.xml.sax.SAXException
+     */
     public Document getXmlDocument(final String id, final String path) throws IOException, SAXException {
-            
-            
-        final URL url = getURL(path);
-        final URLConnection urlConn = url.openConnection();
 
-        if( !hostHeader.equals(host) ){
+        final int priority = Thread.currentThread().getPriority();
+        final URLConnection urlConn = getUrlConnection(id, path);
+
+        if(!hostHeader.equals(host)){
+
             LOG.debug(DEBUG_USING_HOSTHEADER + hostHeader);
             urlConn.addRequestProperty("host", hostHeader);
         }
 
-
         final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder;
+        final DocumentBuilder builder;
 
         try {
             builder = factory.newDocumentBuilder();
-            return builder.parse(urlConn.getInputStream());
 
-        } catch (ParserConfigurationException e) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+            //Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+            final long start = System.nanoTime();
+
+            final Document result = builder.parse(urlConn.getInputStream());
+
+            STATISTICS.get(this.id).addInvocation(System.nanoTime() - start);
+
+            return result;
+
+        } catch (ParserConfigurationException e) {
             throw new IOException(e.getMessage());
+
+        } catch (IOException e) {
+            
+            if( e instanceof SocketTimeoutException){
+                STATISTICS.get(this.id).addReadTimeout();
+            }else if( e instanceof ConnectException){
+                STATISTICS.get(this.id).addConnectTimeout();
+            }else{
+                STATISTICS.get(this.id).addFailure();
+            }
+            
+            // Clean out the error stream. See
+            if(urlConn instanceof HttpURLConnection){
+                cleanErrorStream((HttpURLConnection)urlConn);
+            }
+            throw e;
+
+        }finally{
+
+            Thread.currentThread().setPriority(priority);
+            if(null != urlConn && null != urlConn.getInputStream()){
+                urlConn.getInputStream().close();
+            }
         }
     }
-    
+
+    /**
+     *
+     * @param id
+     * @param path
+     * @return
+     * @throws java.io.IOException
+     */
     public BufferedInputStream getBufferedStream(final String id, final String path) throws IOException {
+
+        final int priority = Thread.currentThread().getPriority();
+        final URLConnection urlConn = getUrlConnection(id, path);
+
+        try{
+            //Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+            final long start = System.nanoTime();
+
+            final BufferedInputStream result = new BufferedInputStream(urlConn.getInputStream());
+
+            STATISTICS.get(this.id).addInvocation(System.nanoTime() - start);
+
+            return result;
+
+        } catch (IOException e) {
             
-        return new BufferedInputStream(getStream(id, path));
+            if( e instanceof SocketTimeoutException){
+                STATISTICS.get(this.id).addReadTimeout();
+            }else if( e instanceof ConnectException){
+                STATISTICS.get(this.id).addConnectTimeout();
+            }else{
+                STATISTICS.get(this.id).addFailure();
+            }
+            
+            // Clean out the error stream. See
+            if(urlConn instanceof HttpURLConnection){
+                cleanErrorStream((HttpURLConnection)urlConn);
+            }
+            throw e;
+
+        }finally{
+
+            Thread.currentThread().setPriority(priority);
+//            if(null != urlConn && null != urlConn.getInputStream()){
+//                urlConn.getInputStream().close();
+//            }
+        }
     }
-    
+
+    /**
+     *
+     * @param id
+     * @param path
+     * @return
+     * @throws java.io.IOException
+     */
     public BufferedReader getBufferedReader(final String id, final String path) throws IOException {
-          
-        return new BufferedReader(new InputStreamReader(getStream(id, path)));
+
+        final int priority = Thread.currentThread().getPriority();
+        final URLConnection urlConn = getUrlConnection(id, path);
+
+        try{
+            //Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+            final long start = System.nanoTime();
+
+            final BufferedReader result = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+            STATISTICS.get(this.id).addInvocation(System.nanoTime() - start);
+
+            return result;
+
+        } catch (IOException e) {
+            
+            if( e instanceof SocketTimeoutException){
+                STATISTICS.get(this.id).addReadTimeout();
+            }else if( e instanceof ConnectException){
+                STATISTICS.get(this.id).addConnectTimeout();
+            }else{
+                STATISTICS.get(this.id).addFailure();
+            }
+            
+            // Clean out the error stream. See
+            if(urlConn instanceof HttpURLConnection){
+                cleanErrorStream((HttpURLConnection)urlConn);
+            }
+            throw e;
+
+        }finally{
+
+            Thread.currentThread().setPriority(priority);
+//            if(null != urlConn && null != urlConn.getInputStream()){
+//                urlConn.getInputStream().close();
+//            }
+        }
     }
-    
+
+    /**
+     *
+     * @param id
+     * @param path
+     * @return
+     * @throws java.io.IOException
+     */
     public long getLastModified(final String id, final String path) throws IOException {
 
         return getUrlConnection(id, path).getLastModified();
     }
-    
+
+    /**
+     *
+     * @param id
+     * @param path
+     * @return
+     * @throws java.io.IOException
+     */
     public boolean exists(final String id, final String path) throws IOException {
 
         boolean success = false;
@@ -128,21 +300,16 @@ public final class HTTPClient {
             con.addRequestProperty("host", hostHeader);
             con.setConnectTimeout(1000);
             con.setReadTimeout(1000);
-            success = HttpURLConnection.HTTP_OK == con.getResponseCode();      
+            success = HttpURLConnection.HTTP_OK == con.getResponseCode();
         }else{
             final File file = new File(path);
             success = file.exists();
         }
         return success;
     }
-    
-    private InputStream getStream(final String id, final String path) throws IOException {
 
-        return getUrlConnection(id, path).getInputStream();
-    }
-    
     private URLConnection getUrlConnection(final String id, final String path) throws IOException {
-            
+
         final URLConnection urlConn = getURL(path).openConnection();
 
         if( !hostHeader.equals(host) ){
@@ -152,22 +319,33 @@ public final class HTTPClient {
 
         return urlConn;
     }
-    
-    
+
+    private void cleanErrorStream(final HttpURLConnection con) throws IOException{
+
+        if(null != con.getErrorStream()){
+            final BufferedReader errReader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            final StringBuilder err = new StringBuilder();
+            for(String line = errReader.readLine(); null != line; line = errReader.readLine()){
+                err.append(line);
+            }
+            LOG.info(err.toString());
+        }
+    }
+
     // --HTTPClient implementation to allow keepalive or pipelining.
     // --  see revision 3596 for original implementation
-    
+
 //    private static final String DEBUG_ADDING_CONF = "Adding configuration ";
-    
+
 //    private static final Map<String,HostConfiguration> hostConfigurations = new HashMap<String,HostConfiguration>();
 //
 //    private static final HttpConnectionManager cMgr = new ConnectionManagerWithoutKeepAlive();
 //    private HttpClient commonsHttpClient;
 
 //    private static final HTTPClient client = new HTTPClient();
-    
-    
-    
+
+
+
 //    private HTTPClient() {
 //        final HttpConnectionManagerParams params = new HttpConnectionManagerParams();
 //        params.setStaleCheckingEnabled(true);
@@ -178,32 +356,32 @@ public final class HTTPClient {
 //        cMgr.setParams(params);
 //        commonsHttpClient = new HttpClient(cMgr);
 //    }
-    
-    
+
+
 //    public static HTTPClient instance(final String id, final String host, final int port) {
-//        
+//
 //        if (!hostConfigurations.containsKey(id)) {
 //            addHostConfiguration(id, host, port);
 //        }
 //    }
-    
+
 //    private HttpMethod executeGet(final String id, final String path) throws IOException {
-//        
+//
 //        final HostConfiguration conf = (HostConfiguration) hostConfigurations.get(id);
 //        final HttpMethod method = new GetMethod(path);
 //        commonsHttpClient.executeMethod(conf, method);
 //        return method;
-//    } 
-    
-    
+//    }
+
+
 //    private void release(final HttpMethod method) {
 //        method.releaseConnection();
 //    }
 //
 //    private synchronized static void addHostConfiguration(final String id, final String host, final int port) {
-//        
+//
 //        if (! hostConfigurations.containsKey(id)) {
-//            
+//
 //            final HostConfiguration conf = new HostConfiguration();
 //            LOG.debug(DEBUG_ADDING_CONF + host + ":" + port);
 //            conf.setHost(host, port, "http");
@@ -211,5 +389,84 @@ public final class HTTPClient {
 //            hostConfigurations.put(id, conf);
 //        }
 //    }
-    
+
+
+
+    private static class Statistic implements Comparable<Statistic>{
+        
+
+        private static final Logger STATISTICS_LOG = Logger.getLogger(Statistic.class);        
+            
+        private final String id;
+        private long totalTime = 0;
+        private long invocations = 0;
+        private volatile long connectTimeouts = 0;
+        private volatile long readTimeouts = 0;
+        private volatile long failures = 0;
+        private static volatile long lastPrint = System.currentTimeMillis() / 60000;
+
+        Statistic(final String id){
+            this.id = id;
+        }
+
+        synchronized void addInvocation(final long time){
+
+            totalTime += (time / 1000000);
+            ++invocations;
+            
+            if(STATISTICS_LOG.isDebugEnabled() && System.currentTimeMillis() / 60000 != lastPrint){ printStatistics(); }
+        }
+        
+        void addFailure(){
+            ++failures;
+        }
+        
+        void addConnectTimeout(){
+            ++connectTimeouts;
+        }
+        
+        void addReadTimeout(){
+            ++readTimeouts;
+        }
+        
+        private long getAverageInvocationTime(){
+            return 0 < invocations ? (totalTime * (long)1000 / invocations) : 0;
+        }
+
+        @Override
+        public String toString() {
+                        
+            return ": " +  new DecimalFormat("000,000,000").format(invocations)
+                    + " : " + new DecimalFormat("000,000,000").format(totalTime)
+                    + "ms : " + new DecimalFormat("0,000,000").format(getAverageInvocationTime())
+                    + "µs : " + new DecimalFormat("00,000").format(failures)
+                    + " :         " + new DecimalFormat("00,000").format(connectTimeouts)
+                    + " : " + new DecimalFormat("00,000").format(readTimeouts)
+                    + " <-- " + id;
+        }
+        
+        public int compareTo(Statistic o) {
+            return (int) (o.getAverageInvocationTime() - getAverageInvocationTime());
+        }
+       
+
+        private static void printStatistics(){
+        
+            final List<Statistic> list = new ArrayList<Statistic>(STATISTICS.values());
+            Collections.sort(list);
+
+            final StringBuilder msg = new StringBuilder();
+            msg.append("\n------ Printing HTTPClient statistics ------\n"
+                    + ": invocations : total time    : average     "
+                    + ":failures: connect errors : read timeouts <- client\n");
+            
+            for(Statistic stat : list){
+                msg.append(stat.toString() + '\n');
+            }
+            msg.append("------ ------------------------------ ------");
+            STATISTICS_LOG.debug(msg.toString());
+            
+            lastPrint = System.currentTimeMillis() / 60000;
+        }        
+    }
 }
