5166a390642c828f4ea3a513d50c545b84ad7dd0
gttersen <gttersen@sesam.no>
2007-05-02 12:46:42 +0000

    Removed unused import that referred to a none existing class
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@4937 cdc3010f-a60b-0410-91f3-d90f869209c8

--- core-api/src/main/java/no/schibstedsok/searchportal/mode/command/BlogSearchCommand.java
+++ core-api/src/main/java/no/schibstedsok/searchportal/mode/command/BlogSearchCommand.java
@@ -8,36 +8,29 @@
 
 package no.schibstedsok.searchportal.mode.command;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import no.schibstedsok.searchportal.query.UrlClause;
 import no.schibstedsok.searchportal.query.LeafClause;
-import no.schibstedsok.searchportal.result.BasicSearchResultItem;
-import no.schibstedsok.searchportal.result.BlogSearchResultItem;
-import no.schibstedsok.searchportal.result.CatalogueSearchResultItem;
+import no.schibstedsok.searchportal.query.UrlClause;
 import no.schibstedsok.searchportal.result.SearchResult;
 import no.schibstedsok.searchportal.result.SearchResultItem;
 
 /**
- *
  * Implementation of blog search command.
- *
+ * <p/>
  * Due to how the blog index is configured queries sent to the index must
  * be applied to two composite fields:
- *
+ * <p/>
  * A search for "sesam bloggsøk" results in the following query sent to the index:
- *
+ * <p/>
  * (content:sesam or extended:sesam) and (content:bloggsøk or extended:bloggsøk)
  *
  * @author maek
  */
 public final class BlogSearchCommand extends AbstractESPFastSearchCommand {
 
-    /** Creates a new instance of FastSearchCommand
+    /**
+     * Creates a new instance of FastSearchCommand
      *
-     * @param cxt Search command context.
+     * @param cxt        Search command context.
      * @param parameters Search command parameters.
      */
     public BlogSearchCommand(final Context cxt) {
@@ -48,23 +41,23 @@ public final class BlogSearchCommand extends AbstractESPFastSearchCommand {
     // Public --------------------------------------------------------
     public SearchResult execute() {
         final SearchResult result = super.execute();
-        for(SearchResultItem item : result.getResults()) {
+        for (SearchResultItem item : result.getResults()) {
             String publishedTime = item.getField("publishedtime");
 
             if (isEpoch(publishedTime)) {
-            	publishedTime = item.getField("httpheaderdate");
+                publishedTime = item.getField("httpheaderdate");
             }
             item.addField("publishedtime", publishedTime);
         }
-        	
+
         if (getParameter("collapse") != null && !getParameter("collapse").equals("")) {
             final SearchResultItem item = result.getResults().get(0);
             String publishedTime = item.getField("publishedtime");
             if (isEpoch(publishedTime)) {
-            	publishedTime = item.getField("httpheaderdate");
+                publishedTime = item.getField("httpheaderdate");
             }
-            if(!isEpoch(publishedTime)) {
-            	result.addField("expandedBlog", item.getField("title"));
+            if (!isEpoch(publishedTime)) {
+                result.addField("expandedBlog", item.getField("title"));
             }
         }
         return result;
@@ -72,13 +65,13 @@ public final class BlogSearchCommand extends AbstractESPFastSearchCommand {
 
 
     protected void visitImpl(final LeafClause clause) {
-        
+
         if (clause.getField() == null && !getTransformedTerm(clause).trim().equals("")) {
-            
+
             appendTermRepresentation(getTransformedTerm(clause));
-            
-        } else if(null != clause.getField() && null == getFieldFilter(clause)) {
-            
+
+        } else if (null != clause.getField() && null == getFieldFilter(clause)) {
+
             appendTermRepresentation(escapeFieldedLeaf(clause));
         }
     }
@@ -101,9 +94,9 @@ public final class BlogSearchCommand extends AbstractESPFastSearchCommand {
         appendToQueryRepresentation(term);
         appendToQueryRepresentation(")");
     }
-    
+
     private boolean isEpoch(String dateString) {
-    	return dateString.startsWith("1970");
+        return dateString.startsWith("1970");
     }
 
 }
