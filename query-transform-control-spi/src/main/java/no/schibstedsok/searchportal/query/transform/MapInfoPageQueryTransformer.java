33824202e8e5aad3b2e415fef2745f416f3a5958
stianh <stianh@sesam.no>
2007-05-14 14:16:57 +0000

    SEARCH-2202 Added MapInfoPageQueryTransformer
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5070 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ query-transform-control-spi/src/main/java/no/schibstedsok/searchportal/query/transform/MapInfoPageQueryTransformer.java
@@ -0,0 +1,36 @@
+// Copyright (2006-2007) Schibsted Søk AS
+package no.schibstedsok.searchportal.query.transform;
+
+import java.util.Map;
+
+import no.schibstedsok.searchportal.datamodel.generic.StringDataObject;
+
+/**
+ * Transformes the query if the requestparameters contains a contentId.
+ * 
+ * @author Stian Hegglund
+ * @version $Revision:$
+ */
+public class MapInfoPageQueryTransformer extends AbstractQueryTransformer {
+      
+    /** Required constructor. */
+    public MapInfoPageQueryTransformer(final QueryTransformerConfig config){
+       
+    }
+    
+    /**
+     * If the request parameteters contains the contentid parameter, append recordid to the query.
+     * 
+     * @see no.schibstedsok.searchportal.query.transform.QueryTransformer
+     */
+    public String getTransformedQuery() {
+        final String originalQuery = getContext().getTransformedQuery();
+        Map<String,StringDataObject> requestParameters = getContext().getDataModel().getParameters().getValues();
+       
+        if(requestParameters != null && requestParameters.containsKey("contentid")){
+            return "recordid:" + originalQuery;
+        }
+        
+        return originalQuery; 
+    }
+}
