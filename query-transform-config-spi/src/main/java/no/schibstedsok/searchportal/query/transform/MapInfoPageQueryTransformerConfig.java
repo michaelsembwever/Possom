262d3fa66d624c465f0cac67ef9bcad40f9a7dc6
stianh <stianh@sesam.no>
2007-05-14 14:14:53 +0000

    SEARCH-2202 Added MapInfoPageQueryTransformerConfig
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5069 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ query-transform-config-spi/src/main/java/no/schibstedsok/searchportal/query/transform/MapInfoPageQueryTransformerConfig.java
@@ -0,0 +1,14 @@
+// Copyright (2006-2007) Schibsted Søk AS
+package no.schibstedsok.searchportal.query.transform;
+
+import no.schibstedsok.searchportal.query.transform.AbstractQueryTransformerConfig.Controller;
+
+/**
+ * 
+ * @author Stian Hegglund
+ * @version $Revision:$
+ */
+@Controller("MapInfoPageQueryTransformer")
+public class MapInfoPageQueryTransformerConfig extends AbstractQueryTransformerConfig{
+
+}
