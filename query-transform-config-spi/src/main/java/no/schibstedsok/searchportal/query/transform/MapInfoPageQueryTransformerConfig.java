98a06ca329854a95086cf6915b9ac9d7edfff64f
ssanjamt <ssanjamt@sesam.no>
2007-05-15 11:02:57 +0000

    added parameterName and prefix attributes
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5083 cdc3010f-a60b-0410-91f3-d90f869209c8

--- query-transform-config-spi/src/main/java/no/schibstedsok/searchportal/query/transform/MapInfoPageQueryTransformerConfig.java
+++ query-transform-config-spi/src/main/java/no/schibstedsok/searchportal/query/transform/MapInfoPageQueryTransformerConfig.java
@@ -2,6 +2,7 @@
 package no.schibstedsok.searchportal.query.transform;
 
 import no.schibstedsok.searchportal.query.transform.AbstractQueryTransformerConfig.Controller;
+import org.w3c.dom.Element;
 
 /**
  * 
@@ -10,5 +11,27 @@ import no.schibstedsok.searchportal.query.transform.AbstractQueryTransformerConf
  */
 @Controller("MapInfoPageQueryTransformer")
 public class MapInfoPageQueryTransformerConfig extends AbstractQueryTransformerConfig{
-
+    private String parameterName = "contentid";
+    private String prefix = "recordid";
+   
+    public final String getParameterName() {
+        return parameterName;
+    }
+    
+    public final String getPrefix() {
+        return prefix;
+    }
+    
+    @Override
+    public MapInfoPageQueryTransformerConfig readQueryTransformer(Element element) {
+        if (element.hasAttribute("parameter-name")){
+            this.parameterName = element.getAttribute("parameter-name");
+        }
+        
+        if (element.hasAttribute("prefix")) {
+            this.prefix = element.getAttribute("prefix");
+        }
+        
+        return this;
+    }
 }
