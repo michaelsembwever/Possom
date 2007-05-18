d0e54d28799eb869ef3067050e621256f7cfc604
michaelsembwever <mick@semb.wever.org>
2007-05-18 21:10:33 +0000

    SEARCH-262 - Build Results SPI  ... one deep rabbit hole this turned into!
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5116 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ data-model-api/src/main/java/no/schibstedsok/searchportal/datamodel/DataModelBeanInfo.java
@@ -0,0 +1,56 @@
+// Copyright (2007) Schibsted Søk AS
+/*
+ * MapDataObjectBeanInfo.java
+ *
+ * Created on 30 January 2007, 20:51
+ *
+ */
+
+package no.schibstedsok.searchportal.datamodel;
+
+import no.schibstedsok.searchportal.datamodel.request.*;
+import no.schibstedsok.searchportal.datamodel.generic.*;
+import java.beans.PropertyDescriptor;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author <a href="mailto:mick@semb.wever.org">Mck</a>
+ * @version <tt>$Id$</tt>
+ */
+public final class DataModelBeanInfo extends MapDataObjectBeanInfo{
+    
+    // Constants -----------------------------------------------------
+    
+    private static final Class BEAN_CLASS = DataModel.class;
+    
+    private static final Logger LOG = Logger.getLogger(DataModelBeanInfo.class);
+    
+    // Attributes ----------------------------------------------------
+    
+    // Static --------------------------------------------------------
+    
+    
+    // Constructors --------------------------------------------------
+    
+    /** Creates a new instance of MapDataObjectBeanInfo */
+    public DataModelBeanInfo() {
+    }
+    
+    // Public --------------------------------------------------------
+    
+    @Override
+    public PropertyDescriptor[] getPropertyDescriptors(){
+        
+      return MapDataObjectBeanInfo.addSingleMappedPropertyDescriptor("search", BEAN_CLASS);
+    }
+    
+    // Package protected ---------------------------------------------
+    
+    // Protected -----------------------------------------------------
+    
+    // Private -------------------------------------------------------
+    
+    // Inner classes -------------------------------------------------
+    
+}
---
+++ data-model-api/src/main/java/no/schibstedsok/searchportal/datamodel/search/SearchDataObject.java
@@ -0,0 +1,70 @@
+/*
+ * SearchDataObject.java
+ * 
+ * Created on 15/05/2007, 11:38:04
+ * 
+ */
+
+package no.schibstedsok.searchportal.datamodel.search;
+
+import no.schibstedsok.searchportal.datamodel.access.AccessAllow;
+import static no.schibstedsok.searchportal.datamodel.access.ControlLevel.*;
+import no.schibstedsok.searchportal.datamodel.generic.DataObject;
+import no.schibstedsok.searchportal.datamodel.query.QueryDataObject;
+import no.schibstedsok.searchportal.result.PagingDisplayHelper;
+import no.schibstedsok.searchportal.result.ResultItem;
+import no.schibstedsok.searchportal.result.ResultList;
+
+/** Contains Search Command and Result information.
+ *
+ * @author <a href="mailto:mick@semb.wever.org">Mck</a>
+ * @version <tt>$Id$</tt>
+ */
+@DataObject
+public interface SearchDataObject {
+    
+//    SearchConfiguration getConfiguration();
+//    void setConfiguration(SearchConfiguration configuration);
+    
+    /**
+     * 
+     * @return 
+     */
+    PagingDisplayHelper getPager();
+    
+    /**
+     * 
+     * @param pager 
+     */
+    void setPager(PagingDisplayHelper pager);
+    
+    /**
+     * 
+     * @return 
+     */
+    ResultList<ResultItem> getResults();
+
+    /**
+     * 
+     * @param results 
+     */
+    @AccessAllow({})
+    void setResults(ResultList<ResultItem> results);
+
+    // QueryDataObject ------------------------------------------------------------
+
+    /**
+     * 
+     * @return 
+     */
+    QueryDataObject getQuery();
+
+    /**
+     * 
+     * @param query 
+     */
+    @AccessAllow({})
+    void setQuery(QueryDataObject query);
+    
+    
+}
