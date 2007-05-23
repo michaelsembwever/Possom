8d3719e1ec51c8db0badb61a4b62fbe9ef70c425
ssmaeklu <ssmaeklu@sesam.no>
2007-05-23 15:36:39 +0000

    The way to get the unique request id changed.
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5159 cdc3010f-a60b-0410-91f3-d90f869209c8

--- core-api/src/main/java/no/schibstedsok/searchportal/mode/command/AbstractESPFastSearchCommand.java
+++ core-api/src/main/java/no/schibstedsok/searchportal/mode/command/AbstractESPFastSearchCommand.java
@@ -184,8 +184,7 @@ public abstract class AbstractESPFastSearchCommand extends AbstractSearchCommand
 
             String sortBy = getSortBy();
 
-            query.setParameter(new SearchParameter(
-                    "sesat:uniqueId",
+            query.setParameter(new SearchParameter("sesat:uniqueId",
                     context.getDataModel().getParameters().getUniqueId()));
 
             query.setParameter(new SearchParameter(BaseParameter.OFFSET, getCurrentOffset(0)));
