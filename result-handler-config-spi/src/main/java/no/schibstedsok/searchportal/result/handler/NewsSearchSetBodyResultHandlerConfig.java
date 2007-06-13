4259861da79a1239d151295ad8f52c83c294cb4e
ssolsagl <ssolsagl@sesam.no>
2007-06-13 12:42:34 +0000

    AGGREG-514
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5317 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ result-handler-config-spi/src/main/java/no/schibstedsok/searchportal/result/handler/NewsSearchSetBodyResultHandlerConfig.java
@@ -0,0 +1,32 @@
+package no.schibstedsok.searchportal.result.handler;
+
+import no.schibstedsok.searchportal.result.handler.AbstractResultHandlerConfig.Controller;
+import org.w3c.dom.Element;
+
+/**
+ * ''Javadoc here.''
+ * <p/>
+ * Created: Jun 13, 2007 1:14:57 PM
+ * Author: Ola MH Sagli <a href="ola@sesam.no">ola at sesam.no</a>
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller("NewsSearchSetBodyResultHandler")
+public class NewsSearchSetBodyResultHandlerConfig extends AbstractResultHandlerConfig {
+
+    private String source;
+
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    @Override
+    public AbstractResultHandlerConfig readResultHandler(final Element element) {
+        source = element.getAttribute("source");
+        return this;
+    }
+}
