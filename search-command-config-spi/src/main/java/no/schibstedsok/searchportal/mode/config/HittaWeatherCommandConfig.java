53446eafe75ec4bb030b8f4c51f01299f1d85171
ssanbern <ssanbern@sesam.no>
2007-06-20 15:04:39 +0000

    add hittweather
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5369 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ search-command-config-spi/src/main/java/no/schibstedsok/searchportal/mode/config/HittaWeatherCommandConfig.java
@@ -0,0 +1,9 @@
+package no.schibstedsok.searchportal.mode.config;
+
+import no.schibstedsok.searchportal.mode.config.CommandConfig.Controller;
+
+
+@Controller("HittaWeatherSearchCommand")
+public class HittaWeatherCommandConfig extends CommandConfig 
+{
+}
