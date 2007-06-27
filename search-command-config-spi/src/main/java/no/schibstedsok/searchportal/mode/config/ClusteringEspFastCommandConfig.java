michaelsembwever/Possom

commit 7d5278922251db251916b75832c79007c61bec78

ESP Search Command now supports static filters (from modes.xml)
git-svn-id: http://sesat.no/svn/sesat-kernel/branches/2.14@5453 cdc3010f-a60b-0410-91f3-d90f869209c8

search-command-config-spi/src/main/java/no/schibstedsok/searchportal/mode/config/EspFastCommandConfig.java
@@ -26,7 +26,7 @@ import org.w3c.dom.NodeList;
  * @version $Id$
  */
 @Controller("ESPFastSearchCommand")
-public class EspFastCommandConfig extends CommandConfig {
+public class EspFastCommandConfig extends FastCommandConfig {
 
     // Constants -----------------------------------------------------
     

search-command-config-spi/src/main/java/no/schibstedsok/searchportal/mode/config/FastCommandConfig.java
@@ -528,7 +528,7 @@ public class FastCommandConfig extends CommandConfig {
     }
 
     @Override
-    public FastCommandConfig readSearchConfiguration(
+    public CommandConfig readSearchConfiguration(
             final Element element,
             final SearchConfiguration inherit) {
         

core-api/src/main/java/no/schibstedsok/searchportal/mode/command/AbstractESPFastSearchCommand.java
@@ -149,8 +149,7 @@ public abstract class AbstractESPFastSearchCommand extends AbstractSearchCommand
                 filterBuilder.append(' ');
             }
 
-
-            final String transformedQuery = getTransformedQuery();
+            final String transformedQuery = appendFilter(cfg.getFilter(), getTransformedQuery());
 
             LOG.debug("Transformed query is " + transformedQuery);
 
@@ -202,6 +201,10 @@ public abstract class AbstractESPFastSearchCommand extends AbstractSearchCommand
             LOG.error(ex.getMessage(), ex);
             throw new InfrastructureException(ex);
         }
+    }
+
+    private String appendFilter(final String filter, final String q) {
+        return filter.length() > 0 ? "and(" + q + "," + "filter(" + filter + "))" : q;
     }
 
     // Z implementation ----------------------------------------------

commit 99ea9910db7f3068a32c1cc3b324e8eec56dab0c

Fixed inheritance structure that I broke in r5453.
git-svn-id: http://sesat.no/svn/sesat-kernel/branches/2.14@5454 cdc3010f-a60b-0410-91f3-d90f869209c8

search-command-config-spi/src/main/java/no/schibstedsok/searchportal/mode/config/ClusteringEspFastCommandConfig.java
@@ -70,7 +70,7 @@ public class ClusteringEspFastCommandConfig extends NewsEspCommandConfig {
 
 
     @Override
-    public CommandConfig readSearchConfiguration(
+    public FastCommandConfig readSearchConfiguration(
             final Element element,
             final SearchConfiguration inherit) {
 

search-command-config-spi/src/main/java/no/schibstedsok/searchportal/mode/config/EspFastCommandConfig.java
@@ -194,7 +194,7 @@ public class EspFastCommandConfig extends FastCommandConfig {
     }
 
     @Override
-    public CommandConfig readSearchConfiguration(
+    public FastCommandConfig readSearchConfiguration(
             final Element element,
             final SearchConfiguration inherit) {
         

search-command-config-spi/src/main/java/no/schibstedsok/searchportal/mode/config/FastCommandConfig.java
@@ -528,7 +528,7 @@ public class FastCommandConfig extends CommandConfig {
     }
 
     @Override
-    public CommandConfig readSearchConfiguration(
+    public FastCommandConfig readSearchConfiguration(
             final Element element,
             final SearchConfiguration inherit) {
         

search-command-config-spi/src/main/java/no/schibstedsok/searchportal/mode/config/NewsAggregatorCommandConfig.java
@@ -141,7 +141,7 @@ public class NewsAggregatorCommandConfig extends ClusteringEspFastCommandConfig 
     }
 
     @Override
-    public CommandConfig readSearchConfiguration(
+    public FastCommandConfig readSearchConfiguration(
             final Element element,
             final SearchConfiguration inherit) {
         

search-command-config-spi/src/main/java/no/schibstedsok/searchportal/mode/config/NewsEspCommandConfig.java
@@ -156,7 +156,7 @@ public class NewsEspCommandConfig extends NavigatableEspFastCommandConfig {
     }
 
     @Override
-    public CommandConfig readSearchConfiguration(
+    public FastCommandConfig readSearchConfiguration(
             final Element element,
             final SearchConfiguration inherit) {
 