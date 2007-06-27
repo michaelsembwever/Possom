10335900a231a4140d71b3927a03c81ae15cef8c
michaelsembwever <mick@semb.wever.org>
2007-06-27 08:18:58 +0000

    DataModel's Access controls were not working correctly
    git-svn-id: http://sesat.no/svn/sesat-kernel/branches/2.14@5443 cdc3010f-a60b-0410-91f3-d90f869209c8

--- data-model-javabean-impl/src/main/java/no/schibstedsok/searchportal/datamodel/BeanDataObjectInvocationHandler.java
+++ data-model-javabean-impl/src/main/java/no/schibstedsok/searchportal/datamodel/BeanDataObjectInvocationHandler.java
@@ -282,7 +282,7 @@ class BeanDataObjectInvocationHandler<T> implements InvocationHandler {
                     disallowed |= cl == level;
                 }
             }
-            if(null != allow && null != disallow && (!allowed || disallowed)){
+            if((null != allow && !allowed) || (null != disallow && disallowed)){
                 throw new IllegalAccessException(ERR_DENIED + method.getName() + " against " + level);
             }
         }
