b39066876baad87fa56b7723ab1ebc0ab299f440
ssmalamb <ssmalamb@sesam.no>
2007-06-28 15:16:28 +0000

    Changed image enrichment to use se_images fast list
    git-svn-id: http://sesat.no/svn/sesat-kernel/branches/2.14@5463 cdc3010f-a60b-0410-91f3-d90f869209c8

--- query-api/src/main/java/no/schibstedsok/searchportal/query/token/TokenPredicate.java
+++ query-api/src/main/java/no/schibstedsok/searchportal/query/token/TokenPredicate.java
@@ -81,6 +81,9 @@ public enum TokenPredicate implements Predicate {
     WIKIPEDIA (Type.FAST),
     EXACT_WIKI (Type.FAST),
 
+    IMAGES (Type.FAST),
+    EXACT_IMAGE (Type.FAST),
+
     PRISJAKT_CATEGORIES_AND_MANUFACTURERS (Type.FAST),
     PRISJAKT_CATEGORIES (Type.FAST),
     PRISJAKT_MANUFACTURERS (Type.FAST),
