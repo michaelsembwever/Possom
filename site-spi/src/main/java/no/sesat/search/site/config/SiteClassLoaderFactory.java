971094f3ca8d38bf37d64835df322cfb739409bf
michaelsembwever <mick@semb.wever.org>
2007-08-23 15:39:24 +0000

    extra debug for SEARCH-3262  & license headers
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5673 cdc3010f-a60b-0410-91f3-d90f869209c8

--- site-spi/src/main/java/no/sesat/search/site/config/SiteClassLoaderFactory.java
+++ site-spi/src/main/java/no/sesat/search/site/config/SiteClassLoaderFactory.java
@@ -1,4 +1,9 @@
-/*
+/* Copyright (2007) Schibsted Søk AS
+ * This file is part of SESAT.
+ * You can use, redistribute, and/or modify it, under the terms of the SESAT License.
+ * You should have received a copy of the SESAT License along with this program.  
+ * If not, see https://dev.sesat.no/confluence/display/SESAT/SESAT+License
+ *
  * SiteClassLoaderFactory.java
  */
 
@@ -32,7 +37,7 @@ public final class SiteClassLoaderFactory implements SiteKeyedFactory {
             = new HashMap<Site, Map<Spi, SiteClassLoaderFactory>>();
     private static final ReentrantReadWriteLock INSTANCES_LOCK = new ReentrantReadWriteLock();
 
-    private static final Logger LOG = Logger.getLogger(SiteConfiguration.class);
+    private static final Logger LOG = Logger.getLogger(SiteClassLoaderFactory.class);
 
     /** The actual class loader for the (site, SPI)-pair */
     private final ClassLoader classLoader;
