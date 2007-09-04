88508a62b437e0b0a5d7a3a0a45dc3090d124185
michaelsembwever <mick@semb.wever.org>
2007-09-04 18:52:14 +0000

    copyright & license
    git-svn-id: http://sesat.no/svn/sesat-kernel/branches/2.15@5750 cdc3010f-a60b-0410-91f3-d90f869209c8

--- war/src/main/java/no/sesat/search/http/servlet/VelocityDebugServlet.java
+++ war/src/main/java/no/sesat/search/http/servlet/VelocityDebugServlet.java
@@ -1,3 +1,9 @@
+/* Copyright (2007) Schibsted Søk AS
+ * This file is part of SESAT.
+ * You can use, redistribute, and/or modify it, under the terms of the SESAT License.
+ * You should have received a copy of the SESAT License along with this program.  
+ * If not, see https://dev.sesat.no/confluence/display/SESAT/SESAT+License
+ */
 package no.sesat.search.http.servlet;
 
 import java.io.File;
