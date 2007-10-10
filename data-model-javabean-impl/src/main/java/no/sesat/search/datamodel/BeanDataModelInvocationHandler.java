05f4009627f0fd6c807dcb25e1f1259984227575
michaelsembwever <mick@semb.wever.org>
2007-10-10 20:55:37 +0000

    SEARCH-3585 - Apply GPL license header to all SESAT (& generic.sesam) source code
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5818 cdc3010f-a60b-0410-91f3-d90f869209c8

--- data-model-javabean-impl/src/main/java/no/sesat/search/datamodel/BeanDataModelInvocationHandler.java
+++ data-model-javabean-impl/src/main/java/no/sesat/search/datamodel/BeanDataModelInvocationHandler.java
@@ -1,8 +1,18 @@
 /* Copyright (2007) Schibsted Søk AS
  * This file is part of SESAT.
- * You can use, redistribute, and/or modify it, under the terms of the SESAT License.
- * You should have received a copy of the SESAT License along with this program.  
- * If not, see https://dev.sesat.no/confluence/display/SESAT/SESAT+License
+ *
+ *   SESAT is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SESAT is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SESAT.  If not, see <http://www.gnu.org/licenses/>.
  */
 /*
  * BeanDataModelInvocationHandler.java
