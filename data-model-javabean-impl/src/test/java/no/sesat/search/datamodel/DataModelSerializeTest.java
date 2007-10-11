8f49d6d0d7e20f7af1c48baf2f74589863b51b28
michaelsembwever <mick@semb.wever.org>
2007-10-11 11:25:11 +0000

    SEARCH-3585 - Apply GPL license header to all SESAT (& generic.sesam) source code
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5821 cdc3010f-a60b-0410-91f3-d90f869209c8

--- data-model-javabean-impl/src/test/java/no/sesat/search/datamodel/DataModelSerializeTest.java
+++ data-model-javabean-impl/src/test/java/no/sesat/search/datamodel/DataModelSerializeTest.java
@@ -1,10 +1,23 @@
-/*
+/** Copyright (2007) Schibsted Søk AS
+ *   This file is part of SESAT.
+ *
+ *   SESAT is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SESAT is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with SESAT.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * DataModelSerializeTest.java
  *
  * Created on 11-Jun-2007, 13:42:18
  *
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
  */
 
 package no.sesat.search.datamodel;
--- query-api/src/main/java/no/sesat/search/query/parser/alt/Alternation.java
+++ query-api/src/main/java/no/sesat/search/query/parser/alt/Alternation.java
@@ -1,10 +1,19 @@
 /* Copyright (2007) Schibsted Søk AS
- * This file is part of SESAT.
- * You can use, redistribute, and/or modify it, under the terms of the SESAT License.
- * You should have received a copy of the SESAT License along with this program.  
- * If not, see https://dev.sesat.no/confluence/display/SESAT/SESAT+License
- */
-/*
+ *   This file is part of SESAT.
+ *
+ *   SESAT is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SESAT is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with SESAT.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * Alternation.java
  *
  *
