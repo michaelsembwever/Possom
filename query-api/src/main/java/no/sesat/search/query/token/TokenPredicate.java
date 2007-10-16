46bdb1f7fa34a087b8b8de0139484b3ba0660ce0
ssmalamb <ssmalamb@sesam.no>
2007-10-16 12:59:12 +0000

    Added six list names for video enrichment
    git-svn-id: http://sesat.no/svn/sesat-kernel/branches/2.15@5829 cdc3010f-a60b-0410-91f3-d90f869209c8

--- query-api/src/main/java/no/sesat/search/query/token/TokenPredicate.java
+++ query-api/src/main/java/no/sesat/search/query/token/TokenPredicate.java
@@ -144,6 +144,21 @@ public enum TokenPredicate implements Predicate {
     /** */
     EXACT_WIKI (Type.FAST),
     /** */
+
+    /** for video */
+    ACTOR (Type.FAST),
+    EXACT_ACTOR (Type.FAST),
+    ARTIST (Type.FAST),
+    EXACT_ARTIST (Type.FAST),
+    FICTION_CHARACTER (Type.FAST),
+    EXACT_FICTION_CHARACTER (Type.FAST),
+    MOTOR_SPORT (Type.FAST),
+    EXACT_MOTOR_SPORT (Type.FAST),
+    MOVIE (Type.FAST),
+    EXACT_MOVIE (Type.FAST),
+    PUBLIC_SERVICE_BROADCASTING (Type.FAST),
+    EXACT_PUBLIC_SERVICE_BROADCASTING (Type.FAST),
+    
     IMAGES (Type.FAST),
     /** */
     EXACT_IMAGE (Type.FAST),
