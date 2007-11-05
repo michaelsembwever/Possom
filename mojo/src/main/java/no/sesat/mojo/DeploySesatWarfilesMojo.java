7babde6d514d1453cb87b6e7460b39905fc7c885
michaelsembwever <mick@semb.wever.org>
2007-11-05 23:20:12 +0000

    SEARCH-3539 - A Better Sesat and Skins build system
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5868 cdc3010f-a60b-0410-91f3-d90f869209c8

--- mojo/src/main/java/no/sesat/mojo/DeploySesatWarfilesMojo.java
+++ mojo/src/main/java/no/sesat/mojo/DeploySesatWarfilesMojo.java
@@ -21,39 +21,72 @@ package no.sesat.mojo;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
 import org.apache.maven.artifact.resolver.ArtifactCollector;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.model.Profile;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.dependency.fromConfiguration.CopyMojo;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.wagon.ConnectionException;
+import org.apache.maven.wagon.ResourceDoesNotExistException;
+import org.apache.maven.wagon.TransferFailedException;
+import org.apache.maven.wagon.Wagon;
+import org.apache.maven.wagon.authentication.AuthenticationException;
+import org.apache.maven.wagon.authorization.AuthorizationException;
+import org.apache.maven.wagon.repository.Repository;
+import org.codehaus.plexus.PlexusConstants;
+import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.archiver.manager.ArchiverManager;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.codehaus.plexus.context.Context;
+import org.codehaus.plexus.context.ContextException;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
 
-/**
+/** Handles deployment of sesat and skins builds.
+ * Deployment differs greatly between the development and the other profiles.
+ * In development profile the behaviour just uses the superclass CopyMojo,
+ *  or the dependency:copy goal, with a precondition that the outputDirectory exists. 
+ *  (avoids a "null" directory created).
+ * In the other profiles, the profile's classifier is used, the artifact downloaded from a remote serverDeployLocation is
+ *  neccessary, and then uploaded to the configured 'profile'DeployRepository which corresponds to the 
+ *  the environments webapp directory. 
+ *  Skins are expected to override these deployRepository settings.
  *
- * @goal local-deploy
+ * @goal deploy
  * @author mick
  * @version $Id$
  */
-public class DeploySesatWarfilesMojo extends CopyMojo{
-    
+public class DeploySesatWarfilesMojo extends CopyMojo implements Contextualizable{
+
     // Constants -----------------------------------------------------
 
     // Attributes ----------------------------------------------------
-    
+
+    private PlexusContainer container;
+
+    private String profile = null;
+
+    /**
+     */
+    private String serverDeployLocation;
+
     // All of these attributes are just explicit overrides to get them into the mojo.
     //  read http://www.mail-archive.com/dev@maven.apache.org/msg60770.html
-    
+
     /**
      * Strip artifact version during copy
-     * 
+     *
      * @parameter expression="${mdep.stripVersion}" default-value="false"
      * @parameter
      */
     private boolean stripVersion = false;
-    
+
     /**
      * Default location used for mojo unless overridden in ArtifactItem
-     * 
+     *
      * @parameter expression="${outputDirectory}"
      *            default-value="${project.build.directory}/dependency"
      * @optional
@@ -63,7 +96,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * Overwrite release artifacts
-     * 
+     *
      * @optional
      * @since 1.0
      * @parameter expression="${mdep.overWriteReleases}" default-value="false"
@@ -72,7 +105,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * Overwrite snapshot artifacts
-     * 
+     *
      * @optional
      * @since 1.0
      * @parameter expression="${mdep.overWriteSnapshots}" default-value="false"
@@ -81,7 +114,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * Overwrite if newer
-     * 
+     *
      * @optional
      * @since 2.0
      * @parameter expression="${mdep.overIfNewer}" default-value="true"
@@ -92,25 +125,25 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
      * Collection of ArtifactItems to work on. (ArtifactItem contains groupId,
      * artifactId, version, type, classifier, location, destFile, markerFile and overwrite.)
      * See "Usage" and "Javadoc" for details.
-     * 
+     *
      * @parameter
      * @required
      * @since 1.0
      */
     private ArrayList artifactItems;
-    
+
     /**
-     * Used to look up Artifacts in the remote repository.
-     * 
+     * Used to look up Artifacts in the remote serverDeployLocation.
+     *
      * @parameter expression="${component.org.apache.maven.artifact.factory.ArtifactFactory}"
      * @required
      * @readonly
      */
     private org.apache.maven.artifact.factory.ArtifactFactory factory;
-    
+
     /**
-     * Used to look up Artifacts in the remote repository.
-     * 
+     * Used to look up Artifacts in the remote serverDeployLocation.
+     *
      * @parameter expression="${component.org.apache.maven.artifact.resolver.ArtifactResolver}"
      * @required
      * @readonly
@@ -119,7 +152,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * Artifact collector, needed to resolve dependencies.
-     * 
+     *
      * @component role="org.apache.maven.artifact.resolver.ArtifactCollector"
      * @required
      * @readonly
@@ -135,8 +168,8 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
     private ArtifactMetadataSource artifactMetadataSource;
 
     /**
-     * Location of the local repository.
-     * 
+     * Location of the local serverDeployLocation.
+     *
      * @parameter expression="${localRepository}"
      * @readonly
      * @required
@@ -145,7 +178,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * List of Remote Repositories used by the resolver
-     * 
+     *
      * @parameter expression="${project.remoteArtifactRepositories}"
      * @readonly
      * @required
@@ -154,7 +187,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * To look up Archiver/UnArchiver implementations
-     * 
+     *
      * @parameter expression="${component.org.codehaus.plexus.archiver.manager.ArchiverManager}"
      * @required
      * @readonly
@@ -163,7 +196,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * POM
-     * 
+     *
      * @parameter expression="${project}"
      * @readonly
      * @required
@@ -172,7 +205,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * Contains the full list of projects in the reactor.
-     * 
+     *
      * @parameter expression="${reactorProjects}"
      * @required
      * @readonly
@@ -181,7 +214,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * If the plugin should be silent.
-     * 
+     *
      * @optional
      * @since 2.0
      * @parameter expression="${silent}" default-value="false"
@@ -190,7 +223,7 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
 
     /**
      * Output absolute filename for resolved artifacts
-     * 
+     *
      * @optional
      * @since 2.0
      * @parameter expression="${outputAbsoluteArtifactFilename}"
@@ -206,47 +239,114 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
     }
 
     // Public --------------------------------------------------------
-    
-    // Z implementation ----------------------------------------------
-    
+
+    // Contextualizable implementation ----------------------------------------------
+
+    public void contextualize(final Context context) throws ContextException {
+
+        container = (PlexusContainer) context.get(PlexusConstants.PLEXUS_KEY);
+    }
+
     // CopyMojo overrides ---------------------------------------------------
-    
+
     public void execute() throws MojoExecutionException{
-        
-        // pre-condition checks
-        
-        // 1. the output directory must exist
-        if(getOutputDirectory().exists()){
-            
-            // 2. output directory is writable
-            if(getOutputDirectory().canWrite()){
-                
-                // execute default behaviour
-                pushFields();
-                super.execute();
-                
-            }else{
-                // 2.failure output directory isn't writable
-                getLog().error( getOutputDirectory().getAbsolutePath() + " can not be written to.");
+
+        serverDeployLocation = project.getProperties().getProperty("serverDeployLocation");
+        pushFields();
+
+        final Wagon wagon = getWagon();
+        if(null != wagon){
+
+            // alpha|nuclei|beta|electron|gamma|production deployment goes through scpexe
+            try{
+
+                @SuppressWarnings("unchecked")
+                final List<ArtifactItem> theArtifactItems = getProcessedArtifactItems(stripVersion);
+
+                for(ArtifactItem item : theArtifactItems){
+
+                    final Artifact artifact = factory.createArtifactWithClassifier(
+                            item.getGroupId(),
+                            item.getArtifactId(),
+                            item.getVersion(),
+                            item.getType(),
+                            profile);
+
+                    resolver.resolve(artifact, getRemoteRepos(), getLocal());
+
+                    final String sesamSite = project.getProperties().getProperty("sesam.site");
+                    final String destName = null != sesamSite
+                            ? sesamSite
+                            : project.getBuild().getFinalName();
+                    
+                    // we are ready to go. but first tag the code
+                    if(Boolean.parseBoolean(project.getProperties().getProperty("tag.on.deploy"))){
+                        
+                    }
+
+                    // now do the upload
+                    getLog().info("Uploading " + artifact.getFile().getAbsolutePath()
+                            + " to " + wagon.getRepository().getUrl() + '/' + destName + ".war");
+
+                    wagon.put(artifact.getFile(), destName + ".war");
+                }
+
+                wagon.disconnect();
+
+            }catch(ConnectionException ex){
+                getLog().error(ex);
+                throw new MojoExecutionException("repository wagon not disconnected", ex);
+            }catch(TransferFailedException ex){
+                getLog().error(ex);
+                throw new MojoExecutionException("transfer failed", ex);
+            }catch(ResourceDoesNotExistException ex){
+                getLog().error(ex);
+                throw new MojoExecutionException("resource does not exist", ex);
+            }catch(AuthorizationException ex){
+                getLog().error(ex);
+                throw new MojoExecutionException("authorisation exception", ex);
+            }catch(ArtifactNotFoundException ex){
+                getLog().error(ex);
+                throw new MojoExecutionException("artifact not found", ex);
+            }catch(ArtifactResolutionException ex){
+                getLog().error(ex);
+                throw new MojoExecutionException("artifact resolution exception", ex);
             }
+
         }else{
-            // 1.failure: the output directory doesn't exist
-            getLog().error( getOutputDirectory().getAbsolutePath() + " does not exist.");
-            final String catalinaBase = System.getProperty("env.CATALINA_BASE");
-            if(null == catalinaBase || 0 == catalinaBase.length()){
-                getLog().info("Define system variable CATALINA_BASE to enable automatic deployment.");
+            // development behaviour comes from super implementation
+            // some pre-condition checks first
+
+            // 1. the output directory must exist
+            if(getOutputDirectory().exists()){
+
+                // 2. output directory is writable
+                if(getOutputDirectory().canWrite()){
+
+                    super.execute();
+
+                }else{
+                    // 2.failure output directory isn't writable
+                    getLog().error(getOutputDirectory().getAbsolutePath() + " can not be written to.");
+                }
+            }else{
+                // 1.failure: the output directory doesn't exist
+                getLog().error(getOutputDirectory().getAbsolutePath() + " does not exist.");
+                final String catalinaBase = System.getProperty("env.CATALINA_BASE");
+                if(null == catalinaBase || 0 == catalinaBase.length()){
+                    getLog().info("Define system variable CATALINA_BASE to enable automatic deployment.");
+                }
             }
         }
-        
-        
+
     }
-    
+
     // Package protected ---------------------------------------------
-    
+
     // Protected -----------------------------------------------------
-    
+
     // Private -------------------------------------------------------
-    
+
     private void pushFields(){
         setArchiverManager(archiverManager);
         setArtifactCollector(artifactCollector);
@@ -254,7 +354,60 @@ public class DeploySesatWarfilesMojo extends CopyMojo{
         setFactory(factory);
         setResolver(resolver);
     }
-    
+
+    /**
+     *
+     * @return the wagon (already connected) to use against the profile's serverDeployLocation 
+     *              or null if in development profile
+     * @throws org.apache.maven.plugin.MojoExecutionException
+     */
+    private Wagon getWagon() throws MojoExecutionException {
+
+        try {
+
+            @SuppressWarnings("unchecked")
+            final List<Profile> profiles = project.getActiveProfiles();
+
+            final Object[] profileMap = new Object[]{"alpha","nuclei","beta","electron","gamma","production"};
+
+            for(Object entry : profileMap){
+                for(Profile p : profiles){
+                    if(p.getId().equals(entry)){
+                        profile = p.getId();
+                        break;
+                    }
+                }
+                if(null != profile){ break; }
+            }
+
+            Wagon wagon = null;
+
+            if(null != profile){
+
+                final String protocol = serverDeployLocation.substring(0, serverDeployLocation.indexOf(':'));
+
+                final Repository wagonRepository = new Repository();
+
+                wagonRepository.setUrl(serverDeployLocation);
+
+                wagon = (Wagon) container.lookup(Wagon.ROLE, protocol);
+                wagon.connect(wagonRepository);
+
+            }
+            return wagon;
+
+        }catch (ConnectionException ex) {
+            getLog().error(ex);
+            throw new MojoExecutionException("repository wagon not connected", ex);
+        }catch (AuthenticationException ex) {
+            getLog().error(ex);
+            throw new MojoExecutionException("repository wagon not authenticated", ex);
+        }catch (ComponentLookupException ex) {
+            getLog().error(ex);
+            throw new MojoExecutionException("repository wagon not found", ex);
+        }
+
+    }
+
     // Inner classes -------------------------------------------------
-   
 }
