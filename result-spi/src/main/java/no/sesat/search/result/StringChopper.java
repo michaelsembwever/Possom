f30003e179bc8434ea23473e3aecffc79e2f028b
ssenrogn <ssenrogn@sesam.no>
2007-11-26 17:27:16 +0000

    Merged revisions 5933 via svnmerge from https://dev.schibstedsok.no/svn/search-portal/branches/2.15
    ........
      r5933 | ssenrogn | 2007-11-26 18:21:16 +0100 (Mon, 26 Nov 2007) | 1 line
      
      Reverted some of my save actions to not clutter up the merge of code.
    ........
    
    
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@5934 cdc3010f-a60b-0410-91f3-d90f869209c8

--- result-spi/src/main/java/no/sesat/search/result/StringChopper.java
+++ result-spi/src/main/java/no/sesat/search/result/StringChopper.java
@@ -1,5 +1,8 @@
 /* Copyright (2005-2007) Schibsted Søk AS
  * This file is part of SESAT.
+ * You can use, redistribute, and/or modify it, under the terms of the SESAT License.
+ * You should have received a copy of the SESAT License along with this program.  
+ * If not, see https://dev.sesat.no/confluence/display/SESAT/SESAT+License
  *
  *   SESAT is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU Affero General Public License as published by
