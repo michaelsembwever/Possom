0c15a77f9c61228a97311534d77cfd4b50b0f231
michaelsembwever <mick@semb.wever.org>
2008-06-20 17:52:59 +0000

    (OPS-204) little extra info, the url, in interceptIOException
    git-svn-id: http://sesat.no/svn/sesat-kernel/branches/2.17@6707 cdc3010f-a60b-0410-91f3-d90f869209c8

--- httpclient-api/src/main/java/no/sesat/search/http/HTTPClient.java
+++ httpclient-api/src/main/java/no/sesat/search/http/HTTPClient.java
@@ -1,4 +1,4 @@
-/* Copyright (2006-2007) Schibsted Søk AS
+/* Copyright (2006-2008) Schibsted Søk AS
  * This file is part of SESAT.
  *
  *   SESAT is free software: you can redistribute it and/or modify
@@ -401,7 +401,7 @@ public final class HTTPClient {
             cleanErrorStream((HttpURLConnection) urlConn);
         }
 
-        LOG.error("IOException occured for server at: " + id);
+        LOG.error("IOException occured for server at: " + id + " (" + urlConn.getURL() + ')');
 
         return ioe;
     }
