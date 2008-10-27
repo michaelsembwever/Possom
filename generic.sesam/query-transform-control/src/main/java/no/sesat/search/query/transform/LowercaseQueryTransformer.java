7230c43bf2ed27779f29dc9ecaa2bfa4fb5e73f6
michaelsembwever <mick@semb.wever.org>
2008-10-27 11:04:59 +0000

    LowercaseQueryTransformer added. Lowercase every character in every term in the query (against the Site's locale).  SEARCH-4933
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@6894 cdc3010f-a60b-0410-91f3-d90f869209c8

---
+++ generic.sesam/query-transform-control/src/main/java/no/sesat/search/query/transform/LowercaseQueryTransformer.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (2008) Schibsted Søk AS
+ * This file is part of SESAT.
+ *
+ *   SESAT is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SESAT is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with SESAT.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package no.sesat.search.query.transform;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import no.sesat.search.query.Clause;
+import no.sesat.search.query.DoubleOperatorClause;
+import no.sesat.search.query.LeafClause;
+import no.sesat.search.query.OperationClause;
+import org.apache.log4j.Logger;
+
+/**
+ * @see LowercaseQueryTransformerConfig
+ *
+ * @version $Id$
+ */
+public final class LowercaseQueryTransformer extends AbstractQueryTransformer {
+
+    private static final Logger LOG = Logger.getLogger(LowercaseQueryTransformer.class);
+
+    public LowercaseQueryTransformer(final QueryTransformerConfig config){}
+
+    /**
+     *
+     * @param clause The clause to prefix.
+     */
+    public void visitImpl(final LeafClause clause) {
+
+        final String term = getTransformedTerms().get(clause);
+
+        if(null != term && term.length()>0){
+
+            getTransformedTerms().put(
+                    clause,
+                    getTransformedTerms().get(clause).toLowerCase(getContext().getSite().getLocale()));
+        }
+    }
+
+    /**
+     *
+     * @param clause The clause to prefix.
+     */
+    public void visitImpl(final DoubleOperatorClause clause) {
+        clause.getFirstClause().accept(this);
+        clause.getSecondClause().accept(this);
+    }
+
+    /**
+     *
+     * @param clause The clause to prefix.
+     */
+    public void visitImpl(final OperationClause clause) {
+        clause.getFirstClause().accept(this);
+    }
+
+    private Map<Clause,String> getTransformedTerms() {
+        return getContext().getTransformedTerms();
+    }
+
+}
