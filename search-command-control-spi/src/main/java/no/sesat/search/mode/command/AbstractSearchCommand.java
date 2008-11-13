1e3d7d46c3565cc224c26d9084fb96a2735fbe42
sshafroi <sshafroi@sesam.no>
2008-11-13 08:33:38 +0000

    revert 6926, compile
    git-svn-id: http://sesat.no/svn/sesat-kernel/trunk@6927 cdc3010f-a60b-0410-91f3-d90f869209c8

--- search-command-control-spi/src/main/java/no/sesat/search/mode/command/AbstractSearchCommand.java
+++ search-command-control-spi/src/main/java/no/sesat/search/mode/command/AbstractSearchCommand.java
@@ -291,7 +291,7 @@ public abstract class AbstractSearchCommand extends AbstractReflectionVisitor im
     // SearchCommand overrides ---------------------------------------------------
 
     public BaseSearchConfiguration getSearchConfiguration() {
-        return context.getSearchConfiguration();
+        return (BaseSearchConfiguration) context.getSearchConfiguration();
     }
 
     /**
